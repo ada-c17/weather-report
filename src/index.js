46e79feef4a85becab4b20a092660248402d7277
Joanna Dudley <jfdudleybmc@gmail.com>
2022-06-08 16:10:33 -0400

    Added color changes to temp button functionality, adjusted color spectrum

--- src/index.js
+++ src/index.js
@@ -8,12 +8,47 @@ const increaseTemp = () => {
   state.temp += 1;
   const tempDisplay = document.getElementById('tempNum');
   tempDisplay.textContent = state.temp;
+  tempDisplay.dispatchEvent(new Event('tempChange'));
 };
 
 const decreaseTemp = () => {
   state.temp -= 1;
   const tempDisplay = document.getElementById('tempNum');
   tempDisplay.textContent = state.temp;
+  tempDisplay.dispatchEvent(new Event('tempChange'));
+};
+
+const changeTempClass = () => {
+  const tempStyling = document.getElementsByClassName('tempStyled');
+  if (state.temp >= 85) {
+    for (let element of tempStyling) {
+      element.className = 'tempStyled tooHot';
+    }
+  } else if (state.temp >= 75 && state.temp <= 84) {
+    for (let element of tempStyling) {
+      element.className = 'tempStyled warm';
+    }
+  } else if (state.temp >= 65 && state.temp <= 74) {
+    for (let element of tempStyling) {
+      element.className = 'tempStyled pleasant';
+    }
+  } else if (state.temp >= 55 && state.temp <= 64) {
+    for (let element of tempStyling) {
+      element.className = 'tempStyled mild';
+    }
+  } else if (state.temp >= 45 && state.temp <= 54) {
+    for (let element of tempStyling) {
+      element.className = 'tempStyled chilly';
+    }
+  } else if (state.temp >= 35 && state.temp <= 44) {
+    for (let element of tempStyling) {
+      element.className = 'tempStyled cold';
+    }
+  } else if (state.temp <= 34) {
+    for (let element of tempStyling) {
+      element.className = 'tempStyled tooCold';
+    }
+  }
 };
 
 const registerEventHandlers = (event) => {
@@ -21,6 +56,8 @@ const registerEventHandlers = (event) => {
   tempUpButton.addEventListener('click', increaseTemp);
   const tempDownButton = document.getElementById('tempDown');
   tempDownButton.addEventListener('click', decreaseTemp);
+  const tempDisplay = document.getElementById('tempNum');
+  tempDisplay.addEventListener('tempChange', changeTempClass);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
