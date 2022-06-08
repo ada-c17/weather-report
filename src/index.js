6f0a4b0aa20e7e067af9c3cbd071259ab4926d94
MandyG415 <missmandygao@gmail.com>
2022-06-08 16:20:46 -0700

    Trying to increase/decrease Temp in Wave 2

---
+++ src/index.js
@@ -0,0 +1,32 @@
+'use strict';
+// const tempColor = (temp) => {
+//   if (temp >= 80) {
+//     //color has to be red
+//   } else if (70 <= temp <= 79) {
+//     //color has to be orange
+//   } else if (60 <= temp <= 69) {
+//     //color is yellow
+//   } else if (50 <= temp <= 59) {
+//     //color is green
+//   } else ( temp <= 49 ) {
+//   } //color is teal
+
+//const getTemp = () =>
+const state = {
+  tempCount: 65,
+};
+
+const tempIncrease = (event) => {
+  //onst newTemp = document.createElement("span");
+  //const tempContainer = document.querySelector("#tempContainer");
+  state.tempCount += 1;
+  const tempCountContainer = document.querySelector('#tempCount');
+  tempCountContainer.textContent = `Temperature is: ${state.tempCount}`;
+};
+
+const registerEventHandlers = (event) => {
+  const raiseTemp = document.querySelector("#raiseTemp");
+  crabButton.addEventListener("click", tempIncrease)
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
