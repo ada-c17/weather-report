afd8bf89dc9fafea33307bd4ec558e03ffab9066
Alexa Coffman <coffman.alexa@gmail.com>
2022-06-09 12:35:20 -0700

    created tempCheck function and added calls to event listeners

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 const temp = {
   fahrenheit: 55,
 };
+const tempCheck = (element) => {
+  if (element.textContent >= 80) {
+    element.className = 'veryHot';
+  } else if (element.textContent >= 70) {
+    element.className = 'hot';
+  } else if (element.textContent >= 60) {
+    element.className = 'warm';
+  } else if (element.textContent >= 50) {
+    element.className = 'cool';
+  } else {
+    element.className = 'cold';
+  }
+};
 
 const getTemp = () => {
   const tempUl = document.getElementById('tempDisplay');
   const tempLi = document.createElement('li');
   tempLi.textContent = temp.fahrenheit;
   tempUl.appendChild(tempLi);
+  tempCheck(tempLi);
 
   const upUl = document.getElementById('increaseTempButton');
   const upLi = document.createElement('li');
   upLi.textContent = '⬆️';
   upLi.addEventListener('click', () => {
     tempLi.textContent = temp.fahrenheit += 1;
+    tempCheck(tempLi);
   });
   upUl.appendChild(upLi);
 
@@ -21,6 +36,7 @@ const getTemp = () => {
   downLi.textContent = '⬇️';
   downLi.addEventListener('click', () => {
     tempLi.textContent = temp.fahrenheit -= 1;
+    tempCheck(tempLi);
   });
   downUl.appendChild(downLi);
 };
