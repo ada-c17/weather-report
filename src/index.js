5fbcfcff8f3baa704d6aa31cddedc1f7e9ce68dc
Jenny <jfperham@gmail.com>
2022-06-09 10:24:34 -0700

    added conditionals to change temp background color based on temp value

--- src/index.js
+++ src/index.js
@@ -1,32 +1,54 @@
 // 'use strict';
 
+// document.getElementById("p2").style.color = "blue";
+
+//write conditional statements to change backgroundColor based on temp
+// 95+	Red
+// 80-95	Orange
+// 70-80	Yellow
+// 55-70	Green
+// 55 or below	Teal
+
+console.log(document.getElementById('temp').innerHTML);
+
 const state = {
-  temp: 60,
+  temp: 70,
 };
+
+const updateTempBackground = () => {
+  if (state.temp >= 90) {
+    document.getElementById('temp').style.backgroundColor = 'red';
+  } else if (state.temp >= 80 && state.temp < 90) {
+    document.getElementById('temp').style.backgroundColor = 'orange';
+  } else if (state.temp >= 70 && state.temp < 80) {
+    document.getElementById('temp').style.backgroundColor = 'green';
+  } else if (state.temp >= 55 && state.temp < 70) {
+    document.getElementById('temp').style.backgroundColor = 'teal';
+  } else if (state.temp < 55) {
+    document.getElementById('temp').style.backgroundColor = '#F2EBE9';
+  }
+};
+
+// document.getElementById('temp').style.backgroundColor = 'red';
 
 const tempUp = () => {
   state.temp += 1;
   const currentTemp = document.querySelector('#temp');
   currentTemp.textContent = `${state.temp} ℉`;
+  updateTempBackground();
 };
-
-// const updateNewBookCount = () => {
-//   state.newBookCount += 1;
-//   const bookCounterElement = document.querySelector('#newBookCount');
-//   bookCounterElement.textContent = `New Books read: ${state.newBookCount}`;
-// };
 
 const tempDown = () => {
   state.temp -= 1;
   const currentTemp = document.querySelector('#temp');
   currentTemp.textContent = `${state.temp} ℉`;
+  updateTempBackground();
 };
 
 const newReport = (event) => {
   console.log('in newReport:', event);
   const titleContainer = document.querySelector('#newCity');
   titleContainer.textContent = document.getElementById('cityToSearch').value;
-  //want to change this text to an h2... how can I do that?
 };
 
 const registerEventHandlers = (event) => {
@@ -40,6 +62,8 @@ const registerEventHandlers = (event) => {
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+//examples:
 
 // const registerEventHandlers = (event) => {
 //   console.log('in event handler:', event);
