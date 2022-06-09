6af5574a63ed890ea02ce9b6f8f77d782bbe2e36
Liqingcathy <cathy.glc@gmail.com>
2022-06-09 01:04:03 -0700

    partial wave2

--- src/index.js
+++ src/index.js
@@ -10,7 +10,56 @@
 3. temperature range changes garden emoji at bottom
 */
 
+const state = {
+  tempIncrement: 77,
+}
 
+const adjustTemp = () => {
+  state.tempIncrement += 1;
+  const tempText = document.querySelector("#temperature");
+  tempText.textContent = `${state.tempIncrement} °C`;
+}
+
+const temperatureNumber = () => {
+  const tempPlus = document.querySelector("#left-arrow-btn");
+  tempPlus.addEventListener('click', () => {
+    state.tempIncrement += 1;
+    if (state.tempIncrement >= 80){
+      document.body.style.backgroundColor = "red";
+    }else if (state.tempIncrement <=79 && state.tempIncrement >= 70){
+      document.body.style.backgroundColor = "orange";
+    }else if (state.tempIncrement <= 69 && state.tempIncrement >= 60){
+      document.body.style.backgroundColor = "yellow";
+    }else if (state.tempIncrement <= 59 && state.tempIncrement >= 50){
+      document.body.style.backgroundColor = "green";
+    }else{
+      document.body.style.backgroundColor = "teal";
+    }
+  })
+}
+
+const temperaturePlusClickHandler = () => {
+  const tempPlus = document.querySelector("#left-arrow-btn");
+  tempPlus.addEventListener("click",  adjustTemp);
+    //change text color and background color
+  // if ( state.tempIncrement > 80){
+  //   document.body.style.backgroundColor = "red";
+  // }
+  
+};
+
+const temperatureMinusClickHandler = () => {
+  const tempMinus = document.querySelector("#right-arrow-btn");
+  tempMinus.addEventListener("click",  () => {
+    state.tempIncrement -= 1;
+    const tempText = document.querySelector("#temperature");
+    tempText.textContent = `${state.tempIncrement} °C`;
+  });
+}
+//content load
+document.addEventListener("DOMContentLoaded", temperaturePlusClickHandler);
+document.addEventListener("DOMContentLoaded", temperatureMinusClickHandler);
+document.addEventListener("DOMContentLoaded", temperatureNumber);
 
 
 /* Wave3 
