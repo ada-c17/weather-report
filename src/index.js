8c201e2eef9ce6f4493f7c36a74c6ee8bdbfdfde
MandyG415 <missmandygao@gmail.com>
2022-06-09 10:12:13 -0700

    Teal color weather

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 'use strict';
-const tempColor = (temp) => {
+const tempColor = () => {
   const colorCode = document.getElementById('tempCount');
   if (temp >= 80) {
     colorCode.style.color = 'red';
-  } else if (70 <= temp <= 79) {
+  } else if (70 <= temp && temp <= 79) {
     //color has to be orange
     colorCode.style.color = 'orange';
-  } else if (60 <= temp <= 69) {
+  } else if (60 <= temp && temp <= 69) {
     //color is yellow
     colorCode.style.color = 'yellow';
-  } else if (50 <= temp <= 59) {
+  } else if (50 <= temp && temp <= 59) {
     //color is green
     colorCode.style.color = 'green';
   } else {
@@ -17,7 +17,7 @@ const tempColor = (temp) => {
   }
 };
 
-document.addEventListener('DOMContentLoaded', tempColor(45));
+document.addEventListener('DOMContentLoaded', tempColor);
 
 const state = {
   tempCount: 65,
