d1b60ed9181f504ad388e1603f9c95c3a0b969e9
Kelly Kang <shkx281@gmail.com>
2022-06-09 09:53:32 -0700

    Added landscape visual

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const state = {
+  temp: 50,
+};
+
+const changeLandscape = (temp) => {
+  const landscapeContainer = document.getElementById('landscapeContainer');
+  if (temp >= 80) {
+    landscapeContainer.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+  } else if (temp >= 70 && temp <= 79) {
+    landscapeContainer.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+  } else if (temp >= 60 && temp <= 69) {
+    landscapeContainer.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+  } else {
+    landscapeContainer.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  }
+};
+
+const increaseTemp = () => {
+  const temperature = document.querySelector('#temperature');
+  state.temp += 1;
+  temperature.textContent = `${state.temp}`;
+  changeLandscape(state.temp);
+};
+
+const decreaseTemp = () => {
+  const temperature = document.querySelector('#temperature');
+  state.temp -= 1;
+  temperature.textContent = `${state.temp}`;
+  changeLandscape(state.temp);
+};
+
+const registerEventHandlers = () => {
+  const upButton = document.querySelector('#tempUpButton');
+  upButton.addEventListener('click', increaseTemp);
+
+  const downButton = document.querySelector('#tempDownButton');
+  downButton.addEventListener('click', decreaseTemp);
+};
+
+if (document.readState !== 'loading') {
+  registerEventHandlers();
+} else {
+  document.addEventListener('DOMContent Loaded', registerEventHandlers);
+}
