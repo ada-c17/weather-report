228b82f9b8a10b02feada21d5af0ce41a2cbc615
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-06-09 10:59:48 -0700

    pulling

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const incrementButton = document.getElementById('tempUp');
+const decrementButton = document.getElementById('tempDown');
+document.getElementById('tempValue') = 60;
+
+incrementButton.addEventListener('click', function () {
+  console.log(tempValue);
+});
