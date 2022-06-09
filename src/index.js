c4fb3a72a7984c2a851ccc15807c55a178b3bc86
MandyG415 <missmandygao@gmail.com>
2022-06-09 10:53:56 -0700

    Still trying to change temp color

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 'use strict';
-const tempColor = () => {
+const tempColor = (temp) => {
   const colorCode = document.getElementById('tempCount');
   if (temp >= 80) {
     colorCode.style.color = 'red';
@@ -17,30 +17,55 @@ const tempColor = () => {
   }
 };
 
-document.addEventListener('DOMContentLoaded', tempColor);
-
 const state = {
   tempCount: 65,
 };
 
-const tempIncrease = (event) => {
+const tempIncrease = () => {
   state.tempCount += 1;
+  console.log(state.tempCount);
   const tempCountContainer = document.querySelector('#tempCount');
   tempCountContainer.textContent = `${state.tempCount}`;
+  // tempColor(state.tempCount);
+  // console.log("Hello");
+  const colorCode = document.getElementById('tempCount');
+  if (state.tempCount >= 80) {
+    colorCode.style.color = 'red';
+    console.log("Red");
+  } else if (70 <= state.tempCount && state.tempCount <= 79) {
+    //color has to be orange
+    colorCode.style.color = 'orange';
+    console.log("orange");
+  } else if (60 <= state.tempCount && state.tempCount <= 69) {
+    //color is yellow
+    colorCode.style.color = 'yellow';
+    console.log("yellow");
+  } else if (50 <= state.tempCount && state.tempCount <= 59) {
+    //color is green
+    colorCode.style.color = 'green';
+    console.log("green");
+  } else {
+    colorCode.style.color = 'teal';
+  }
+  console.log("hello");
 };
 
-const tempDecrease = (event) => {
+const tempDecrease = () => {
   state.tempCount -= 1;
   const tempCountContainer = document.querySelector('#tempCount');
   tempCountContainer.textContent = `${state.tempCount}`;
+  tempColor(state.tempCount);
+  console.log("hegood morningllo");
 };
 
 const registerEventHandlers = (event) => {
   const raiseTemp = document.querySelector('#raiseTemp');
   raiseTemp.addEventListener('click', tempIncrease);
+  raiseTemp.addEventListener('click', tempColor);
 
   const decreaseTemp = document.querySelector('#decreaseTemp');
   decreaseTemp.addEventListener('click', tempDecrease);
+  decreaseTemp.addEventListener('click', tempColor);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
