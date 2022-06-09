1c4f6b9c5c8091a986256b0a8766b03387a99441
Alexa Coffman <coffman.alexa@gmail.com>
2022-06-09 12:06:48 -0700

    added up button, event listener not working

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const temp = {
 };
 
 const increaseTemp = () => {
-  temp.fahrenheit += 1;
+  temp += 1;
 };
 
 const decreaseTemp = () => {
@@ -14,8 +14,13 @@ const getTemp = () => {
   const tempUl = document.getElementById('tempDisplay');
   const tempLi = document.createElement('li');
   tempLi.textContent = temp.fahrenheit;
-
   tempUl.appendChild(tempLi);
+
+  const upUl = document.getElementById('increaseTempButton');
+  const upLi = document.createElement('li');
+  upLi.textContent = '⬆️';
+  upLi.addEventListener('click', increaseTemp);
+  upUl.appendChild(upLi);
 };
 
 if (document.readyState !== 'loading') {
