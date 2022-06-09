a982125777da82d02c113b10aaea13e09404572b
Myranae <myranae@pm.me>
2022-06-09 11:50:31 -0400

    aligns main text items; adds background assets to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 'use strict';
+import 'dotenv/config';
+import express from 'express';
+
+require('dotenv').config();
+
+/* from reading
+
+
+
+const axios = require('axios');
+
+const printSucess = (response) => {
+  console.log('success', response.data);
+};
+
+const printError = (error) => {
+  console.log('error', error.response.data);
+};
+
+axios
+  .get('https://us1.locationiq.com/v1/search.php', {
+    params: {
+      key: process.env['API_KEY'],
+      q: 'Seattle, Washington, USA',
+      format: 'json',
+    }
+  })
+  .then(printSuccess)
+  .catch(printError)
+  .finally(() => {
+    console.log('this is always exectued, no matter what!)
+  })
+
+
+
+
+
+  */
