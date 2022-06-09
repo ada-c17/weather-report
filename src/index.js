ff9fb3ad33a29ea49ecad39880a33cb555926c41
maria <camilatagleo@gmail.com>
2022-06-09 11:37:20 -0700

    TempCount color changing Ok, wave 02

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@
 const tempColor = (temp) => {
   const colorCode = document.getElementById('tempCount');
   if (temp >= 80) {
-    colorCode.style.color = 'red';
+    colorCode.className = 'hotTemp';
   } else if (70 <= temp && temp <= 79) {
-    //color has to be orange
-    colorCode.style.color = 'orange';
+    colorCode.className = 'warmTemp';
   } else if (60 <= temp && temp <= 69) {
-    //color is yellow
-    colorCode.style.color = 'yellow';
+    colorCode.className = 'mildTemp';
   } else if (50 <= temp && temp <= 59) {
-    //color is green
-    colorCode.style.color = 'green';
-  } else {
-    colorCode.style.color = 'teal';
+    colorCode.className = 'chillTemp';
+  } else if (-100 <= temp && temp <= 49) {
+    colorCode.className = 'coldTemp';
   }
 };
 
@@ -23,31 +20,9 @@ const state = {
 
 const tempIncrease = () => {
   state.tempCount += 1;
-  console.log(state.tempCount);
   const tempCountContainer = document.querySelector('#tempCount');
   tempCountContainer.textContent = `${state.tempCount}`;
-  // tempColor(state.tempCount);
-  // console.log("Hello");
-  const colorCode = document.getElementById('tempCount');
-  if (state.tempCount >= 80) {
-    colorCode.style.color = 'red';
-    console.log("Red");
-  } else if (70 <= state.tempCount && state.tempCount <= 79) {
-    //color has to be orange
-    colorCode.style.color = 'orange';
-    console.log("orange");
-  } else if (60 <= state.tempCount && state.tempCount <= 69) {
-    //color is yellow
-    colorCode.style.color = 'yellow';
-    console.log("yellow");
-  } else if (50 <= state.tempCount && state.tempCount <= 59) {
-    //color is green
-    colorCode.style.color = 'green';
-    console.log("green");
-  } else {
-    colorCode.style.color = 'teal';
-  }
-  console.log("hello");
+  tempColor(state.tempCount);
 };
 
 const tempDecrease = () => {
@@ -55,7 +30,6 @@ const tempDecrease = () => {
   const tempCountContainer = document.querySelector('#tempCount');
   tempCountContainer.textContent = `${state.tempCount}`;
   tempColor(state.tempCount);
-  console.log("hegood morningllo");
 };
 
 const registerEventHandlers = (event) => {
