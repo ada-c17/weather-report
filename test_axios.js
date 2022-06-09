a3973ae038478b41b1fe778977e036a846def77c
joank <jakovacs@protonmail.com>
2022-06-09 10:41:25 -0700

    Adds test axios file as proof of concept for API calls

---
+++ test_axios.js
@@ -0,0 +1,63 @@
+const axios = require("axios");
+
+
+const serverAddress = "http://127.0.0.1:5000";
+const city = "Ballard";
+let longitude=0;
+let latitude=0;
+
+
+
+const parseWeatherReport = function(weatherData) {
+  const tempKelvin = weatherData.current.temp;
+  const tempFahrenheit = ((tempKelvin-273.15)*1.8)+32;
+  const skyDescription = weatherData.current.weather[0].description;
+  const cloudCoverage = `${weatherData.current.clouds}%`;
+
+
+  console.log("temp in Fahrenheit:  ", Math.floor(tempFahrenheit));
+  console.log("weather description:  ", skyDescription);
+  console.log("cloud cover:  ", cloudCoverage);
+};
+
+//axios call for long and lat
+axios.get(`${serverAddress}/location`, {
+    params: {
+      q:city,
+    },
+  })
+  .then((response) => {
+    console.log(response.status);
+    longitude = response.data[0].lon;
+    latitude = response.data[0].lat;
+
+
+
+
+
+    axios.get(`${serverAddress}/weather`, {
+        params: {
+          lat:latitude,
+          lon:longitude,
+        },
+      })
+      .then((response) => {
+        console.log(response.status);
+        parseWeatherReport(response.data);
+      })
+      .catch((response) => {
+        console.log(response.status);
+        console.log("Mother fucking fuck")
+      });
+
+
+
+
+  })
+  .catch((response) => {
+    console.log(response.status);
+    console.log("You FUCKED UP");
+  })
+  .finally(() => {
+    console.log("okay all done! ^-^7");
+  });
