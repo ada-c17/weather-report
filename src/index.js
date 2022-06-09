d545e55fc208cc0282b23bd25a67b5e6b27b9451
Liqingcathy <cathy.glc@gmail.com>
2022-06-09 00:07:36 -0700

    added comment for each wave

---
+++ src/index.js
@@ -0,0 +1,42 @@
+/* Wave2 
+1. increase and decrease temperature on click 
+2. temperature # change text color and background color accordingly
+    Temp(F)	Color     landscape
+    80+	    Red       🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
+    70-79	  Orange    🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷
+    60-69	  Yellow    🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃
+    50-59	  Green     🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲
+    49 or   below	Teal 🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲
+3. temperature range changes garden emoji at bottom
+*/
+
+
+
+
+/* Wave3 
+An element that displays a city name
+A text input element that allows the user to change the city name
+
+*/
+
+
+
+/** Wave4
+  Calling API
+ 
+ */
+
+
+/** Wave5
+ Option	Sky
+  Sunny	"☁️ ☁️ ☁️ ☀️ ☁️ ☁️"
+  Cloudy	"☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️"
+  Rainy	"🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧"
+  Snowy	"🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨"
+*/
+
+
+/** Wave6
+  Resetting the City Name
+  
+ */
