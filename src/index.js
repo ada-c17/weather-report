c68b26c457837ac8251d66f6ede8e8d220885aeb
Nikki Torab <nikkitorab@gmail.com>
2022-06-09 12:02:47 -0700

    wave 2 buttons work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-<script src="./node_modules/axios/dist/axios.min.js"></script>
+'use strict';
+// const axios = require('axios');
+
+const increaseTemp = () => {
+  let temp = parseInt(document.getElementById('temp-value').textContent);
+  temp++;
+  document.getElementById('temp-value').textContent = temp;
+
+  if (temp >= 80) {
+  }
+};
+
+const decreaseTemp = () => {
+  let temp = parseInt(document.getElementById('temp-value').textContent);
+  temp--;
+  document.getElementById('temp-value').textContent = temp;
+};
+
+const upEvent = () => {
+  const upButton = document.getElementById('up-button');
+  upButton.addEventListener('click', increaseTemp);
+};
+
+const downEvent = () => {
+  const downButton = document.getElementById('down-button');
+  downButton.addEventListener('click', decreaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', upEvent);
+document.addEventListener('DOMContentLoaded', downEvent);
