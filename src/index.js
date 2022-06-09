39efff469f827e4d132d5e834234a72a4bd830ce
MandyG415 <missmandygao@gmail.com>
2022-06-09 12:03:35 -0700

    Committing before push

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const tempColor = (temp) => {
   const colorCode = document.getElementById('tempCount');
-  if (temp >= 80) {
+  if (temp.innerHTML >= 80) {
     colorCode.style.color = 'red';
   } else if (70 <= temp && temp <= 79) {
     //color has to be orange
@@ -28,26 +28,6 @@ const tempIncrease = () => {
   tempCountContainer.textContent = `${state.tempCount}`;
   // tempColor(state.tempCount);
   // console.log("Hello");
-  const colorCode = document.getElementById('tempCount');
-  if (state.tempCount >= 80) {
-    colorCode.style.color = 'red';
-    console.log("Red");
-  } else if (70 <= state.tempCount && state.tempCount <= 79) {
-    //color has to be orange
-    colorCode.style.color = 'orange';
-    console.log("orange");
-  } else if (60 <= state.tempCount && state.tempCount <= 69) {
-    //color is yellow
-    colorCode.style.color = 'yellow';
-    console.log("yellow");
-  } else if (50 <= state.tempCount && state.tempCount <= 59) {
-    //color is green
-    colorCode.style.color = 'green';
-    console.log("green");
-  } else {
-    colorCode.style.color = 'teal';
-  }
-  console.log("hello");
 };
 
 const tempDecrease = () => {
@@ -55,7 +35,7 @@ const tempDecrease = () => {
   const tempCountContainer = document.querySelector('#tempCount');
   tempCountContainer.textContent = `${state.tempCount}`;
   tempColor(state.tempCount);
-  console.log("hegood morningllo");
+  console.log('hegood morningllo');
 };
 
 const registerEventHandlers = (event) => {
