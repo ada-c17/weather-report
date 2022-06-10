82035de475883479114d7f69bfb451fefb9f3a0b
Sreymompech <sanchhen2013@gmail.com>
2022-06-09 17:34:09 -0700

    Adding code for changing text color and background of temp, and landscape of garden

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-const state = {
+let state = {
   temp: 75,
+};
+
+const changColor = () => {
+  const tempColor = document.getElementById('temp');
+  const gardenLandScape = document.getElementById('land-scape');
+  const landScaps = [
+    '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂',
+    '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷',
+    '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃',
+    '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲',
+  ];
+
+  console.log('hello world');
+  if (state.temp > 79) {
+    tempColor.className = 'temp-80';
+    console.log(tempColor.className);
+    gardenLandScape.textContent = `${landScaps[0]}`;
+  } else if (state.temp > 69) {
+    tempColor.className = 'temp-70';
+    gardenLandScape.textContent = `${landScaps[1]}`;
+  } else if (state.temp > 59) {
+    tempColor.className = 'temp-60';
+    gardenLandScape.textContent = `${landScaps[2]}`;
+  } else if (state.temp > 49) {
+    tempColor.className = 'temp-50';
+    gardenLandScape.textContent = `${landScaps[3]}`;
+  } else {
+    tempColor.className = 'temp-49';
+    gardenLandScape.textContent = `${landScaps[3]}`;
+  }
 };
 
 const increaseTemp = (event) => {
   state.temp += 1;
   const increaseTempContainer = document.querySelector('#temp');
   increaseTempContainer.textContent = `${state.temp}`;
+  changColor();
 };
 
 const decreaseTemp = (event) => {
   state.temp -= 1;
   const decreaseTempContainer = document.querySelector('#temp');
   decreaseTempContainer.textContent = `${state.temp}`;
+  changColor();
 };
 
 const registerEventHandlers = (event) => {
