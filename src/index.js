bb7c2b36994645a1e6f5cc6bd7b74ef4501dc23a
Julie <jawarren89@gmail.com>
2022-06-09 18:50:42 -0700

    changes temp color according to current temp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ const state = {
   temp: parseInt(document.getElementById('display-temp').innerHTML),
 };
 
+const changeTempColor = () => {
+  const currentTemp = document.getElementById('display-temp');
+  if (state.temp > 80) {
+    currentTemp.style.color = 'red';
+  } else if (state.temp >= 70) {
+    currentTemp.style.color = 'orange';
+  } else if (state.temp >= 60) {
+    currentTemp.style.color = 'yellow';
+  } else if (state.temp >= 50) {
+    currentTemp.style.color = 'green';
+  } else if (state.temp <= 49) {
+    currentTemp.style.color = 'teal';
+  }
+};
+
 const changeTemp = () => {
   const currentTemp = document.getElementById('display-temp');
   const arrowUpButton = document.getElementById('arrow-up');
@@ -11,10 +26,12 @@ const changeTemp = () => {
   arrowUpButton.addEventListener('click', () => {
     state.temp += 1;
     currentTemp.textContent = state.temp;
+    changeTempColor();
   });
   arrowDownButton.addEventListener('click', () => {
     state.temp -= 1;
     currentTemp.textContent = state.temp;
+    changeTempColor();
   });
 };
 
