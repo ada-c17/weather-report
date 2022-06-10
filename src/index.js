78d56c0aa35a1e5c60daeddf1fe7fd0d799d2e0c
olive <olivealavine@gmail.com>
2022-06-09 17:10:04 -0700

    started js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const temperature = document.getElementById('tempDisplay');
+console.log(typeof parseInt(temperature.textContent));
+
+const state = {
+  clickCount: parseInt(temperature.textContent),
+};
+
+const increaseTemp = () => {
+  const temperature = document.getElementById('tempDisplay');
+  state.clickCount += 1;
+  temperature.textContent = state.clickCount;
+};
+
+// if button up clicked, textcontet +=1
+//  if down clicked, textcontent -= 1
+
+const registerEventHandlers = () => {
+  const upButton = document.getElementById('up');
+  upButton.addEventListener('click', increaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
