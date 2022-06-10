4f53224457b3c5c5132105a872aa09dd48f0904a
Ruge Li <rugeli0605@gmail.com>
2022-06-09 21:23:10 -0700

    sky background set

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('conneted');
+console.log('connected');
+
+const dropBar = document.querySelector('#sky-selector');
+const skyBackground = document.querySelector('#sky-box');
+
+const getSelectedSky = (backgrounds) => {
+  const sky = dropBar.options[dropBar.selectedIndex].text;
+  let background;
+  switch (true) {
+    default:
+      background = backgrounds[0];
+      break;
+    case sky === "Sunny":
+      background = backgrounds[1];
+      break;
+    case sky === "Cloudy":
+      background = backgrounds[2];
+      break;
+    case sky === "Rainy":
+      background = backgrounds[3];
+      break;
+    case sky === "Snowy":
+      background = backgrounds[4];
+      break;
+    case sky === "Windy":
+      background = backgrounds[5];
+  
+  }
+  return background;
+};
+
+const changeSkyBackground = () => {
+  const imgs = [
+    '/assets/defaultSky.jpeg',
+    '/assets/sunny.jpeg',
+    '/assets/cloudy.jpeg',
+    '/assets/rainy.jpeg',
+    '/assets/snowy.jpeg',
+    '/assets/windy.jpeg'
+  ];
+  const img = getSelectedSky(imgs);
+  skyBackground.style.background = `url(${img})`;
+};
