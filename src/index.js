33c8a1fa801b3d5387f38725b0600fd5d309839a
Hope Olaide <hopeolaide@gmail.com>
2022-06-10 19:13:52 -0400

    Updates to wave02.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,64 @@
 'use strict';
 
 const state = {
-  temperature: 0,
-}; 
+  temperature: 0
+};
+
+const domElements = {
+  currentTemp: document.querySelector("#currentTemperature"),
+}
+
+const render = {
+  temperature: function () {
+    domElements.currentTemp.textContent = state.temperature;
+  }
+}
+
+// if (temperature > 40) {
+ 
+// }
 
 // Math.floor(1.8 * (k - 273) + 32)
 
+const weatherChange = (temperature) => {
+  // What season goes with this temperature?
+  // Given a temperature access the season data structure 
+  // Set uppper and lower bounds
+  const weatherReference = [
+    {
+      upperTemp: 80,
+      lowerTemp:70,
+      color: 'red'
+    },
+    {
+      upperTemp: 69,
+      lowerTemp:60,
+      color: 'orange'
+    },
+    {
+      upperTemp: 59,
+      lowerTemp: 50,
+      color: 'yellow'
+    }
+  ]
+}
+
 const increaseTemp = () => {
   state.temperature += 1;
-  console.log(state.temperature);
+  // Ask about the temperature change 
+  weatherChange(state.temperature);
+  render.temperature();
 };
-console.log(state.temperature);
+// console.log(state.temperature);
 
 const decreaseTemp = () => {
   state.temperature -= 1;
-  console.log(state.temperature);
+// Ask about the temperature change 
+  weatherChange(state.temperature);
+  render.temperature();
+  
 };
-console.log(state.temperature);
+// console.log(state.temperature);
 
 
 const registerEventHandlers = () => {
@@ -27,3 +69,10 @@ const registerEventHandlers = () => {
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
+
+const renderTemperature = () => {
+  const temperatureContainer = document.querySelector("#currentTemperature");
+  temperatureContainer.textContent = state.temperature;
+}
+
+// document.body.style.backgroundColor = "navy";
