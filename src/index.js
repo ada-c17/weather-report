97585074fdc81f9c535f4b95693879b86ceeb337
sarah <skiyoko14@gmail.com>
2022-06-10 00:04:56 -0700

    wave-05 sky selector changes sky

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+const GARDENSKIES = {
+  sunny: '☁️ ☁️ ☁️ ☀️ ☁️ ☁️',
+  cloudy: '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️',
+  rainy: '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧',
+  snowy: '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨',
+};
+
 const updateTitleCity = () => {
   let titleCity = document.querySelector('#titleCity');
   console.log(cityName.value);
   titleCity.textContent = `${cityName.value}`;
 };
 
+const updateWeatherGardenSky = () => {
+  let gardenSky = document.querySelector('#gardenSky');
+  console.log(`${weatherSelector.value}`);
+  gardenSky.textContent = GARDENSKIES[weatherSelector.value];
+};
+
 const registerEventHandlers = () => {
   const cityInput = document.querySelector('#cityName');
   cityInput.addEventListener('input', updateTitleCity);
+
+  const weatherSelector = document.querySelector('#weatherSelector');
+  weatherSelector.addEventListener('change', updateWeatherGardenSky);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
