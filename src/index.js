4fac8fd14fd19e1db8207215d9147da632ae8c36
Saria Pastor <saria.pastor@protonmail.com>
2022-06-09 22:42:08 -0400

    Outlines all page sections, implements basic version of wave 2 functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
 'use strict';
+
+const landscapes = {
+  hot: '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂',
+  warm: '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷',
+  goldilocks: '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃',
+  cool: '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲',
+  cold: '❄️',
+};
+
+const weather = {
+  temperature: 72,
+};
+
+const tempClass = (temp) => {
+  if (temp > 87) {
+    return 'hot';
+  }
+  if (temp > 77) {
+    return 'warm';
+  }
+  if (temp > 67) {
+    return 'goldilocks';
+  }
+  if (temp > 57) {
+    return 'cool';
+  }
+  return 'cold';
+};
+
+const changeTemp = (e) => {
+  console.log(e.target.id);
+  e.target.id === 'heat' ? weather.temperature++ : weather.temperature--;
+  document.querySelector('#temp-display h1').textContent = weather.temperature;
+  document.getElementById('temp-display').classList = `${tempClass(
+    weather.temperature
+  )}`;
+  document.querySelector('#landscape h1').textContent =
+    landscapes[tempClass(weather.temperature)];
+};
+
+const registerEventHandlers = () => {
+  const controls = document.getElementsByClassName('temp-control');
+  for (const control of controls) {
+    control.addEventListener('click', changeTemp);
+  }
+  // document.getElementById("cool").addEventListener("click",decreaseTemp);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
