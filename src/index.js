087ec06e8df20b9fbf8ba09db23b13805094d4dc
Jande Royalty <janderoyalty@gmail.com>
2022-06-10 04:05:19 -0500

    I believe i have messed this up really badly. Ask for help in the morning

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-const state = {
-  temp: 70,
+// const skyCondition = document.querySelector('#sky');
+
+console.log(document.getElementById('select-sky-box').value);
+// console.log(state.skyCondition);
+// console.log(document.getElementsByClassName('.sky'));
+
+const changeSky = () => {
+  if (document.getElementById('select-sky-box').value === 'sunny') {
+    document.querySelector('#sky').style.background =
+      "url('/assets/sky/sunny 1920x360.jpg')";
+  } else if (document.getElementById('select-sky-box').value === 'rainy') {
+    document.querySelector('#sky').style.background =
+      "url('/assets/sky/rainy 1920x360.jpg')";
+  } else if (document.getElementById('select-sky-box').value === 'cloudy') {
+    document.querySelector('#sky').style.background =
+      "url('/assets/sky/cloudy 1920x360.jpg')";
+  } else if (document.getElementById('select-sky-box').value === 'snowy') {
+    document.querySelector('#sky').style.background =
+      "url('/assets/sky/snowy 1920x360.jpg')";
+  }
 };
+
+const registerEventHandlers = () => {
+  const makeSkyChange = document.getElementById('select-sky-box');
+  makeSkyChange.addEventListener('change', changeSky);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
