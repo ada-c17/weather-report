4e24e9b53cb428b5b73a35cdecf73d9fe4c85a4c
Sreymompech <sanchhen2013@gmail.com>
2022-06-10 10:41:13 -0700

    Add Wave 5 and 6

--- src/index.js
+++ src/index.js
@@ -47,14 +47,20 @@ const decreaseTemp = (event) => {
 };
 
 const registerEventHandlers = (event) => {
-  const increaseTempButton = document.querySelector('#increase-temp');
+  const increaseTempButton = document.getElementById('increase-temp');
   increaseTempButton.addEventListener('click', increaseTemp);
 
-  const decreaseTempButton = document.querySelector('#decrease-temp');
+  const decreaseTempButton = document.getElementById('decrease-temp');
   decreaseTempButton.addEventListener('click', decreaseTemp);
 
   const cityName = document.getElementById('city-name');
   cityName.addEventListener('input', getCityName);
+
+  const selectSky = document.getElementById('sky');
+  selectSky.addEventListener('change', changeSky);
+
+  const changeReset = document.getElementById('reset-btn');
+  changeReset.addEventListener('click', resetCity);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
@@ -65,4 +71,72 @@ const getCityName = () => {
   const cityName = document.getElementById('city-name').value;
   const changeCity = document.getElementById('header-city');
   changeCity.textContent = `for the city of ${cityName}`;
+};
+
+// Wave 5
+const skyEmoji = [
+  '☁️ ☁️ ☁️ ☀️ ☁️ ☁️',
+  '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️',
+  '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧',
+  '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨',
+];
+
+const changeSky = () => {
+  const selectValue = document.getElementById('sky').value;
+  const gardenSky = document.getElementById('garden-sky');
+  if (selectValue === 'Sunny') {
+    gardenSky.textContent = `${skyEmoji[0]}`;
+  } else if (selectValue === 'Cloudy') {
+    gardenSky.textContent = `${skyEmoji[1]}`;
+  } else if (selectValue === 'Rainy') {
+    gardenSky.textContent = `${skyEmoji[2]}`;
+  } else {
+    gardenSky.textContent = `${skyEmoji[3]}`;
+  }
+};
+
+// Wave 6
+const resetCity = () => {
+  const cityName = document.getElementById('city-name');
+  const changeCity = document.getElementById('header-city');
+  cityName.value = '';
+  changeCity.textContent = 'for the city of Seattle';
+};
+
+//Wave 4
+//const axios = require('axios');
+const getRealtimeTemp = () => {
+  const cityName = document.getElementById('city-name').value;
+  axios
+    .get('https://us1.locationiq.com/v1/search', {
+      params: {
+        key: process.env['LOCATION_KEY'],
+        q: cityName,
+        format: 'json',
+      },
+    })
+    .then((response) => {
+      const searchLocation = response.data[0];
+      const latitudeCity = searchLocation.lat;
+      const longtitudeCity = searchLocation.longtitudeCity;
+      axios
+        .get('https://us1.locationiq.com/v1/search', {
+          params: {
+            key: process.env['WEATHER_KEY'],
+            lat: latitudeCity,
+            lon: longtitudeCity,
+            format: 'json',
+          },
+        })
+        .then((temResponse) => {
+          const currentTemp = temResponse.current.temp;
+          const temp = document.getElementById('temp');
+          temp.textContent = `${currentTemp}`;
+          const realTemp = document.getElementById('realtime-temp');
+          realTemp.addEventListener('click', getRealtimeTemp);
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
