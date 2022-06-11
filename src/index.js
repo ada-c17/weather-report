8c476c0534fd5ff85a6bc2f6cb80fae022b0831b
Julie <jawarren89@gmail.com>
2022-06-10 20:31:10 -0700

    reattempt proxy server and venv, delete incorrect venv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+const LOCATIONIQ_KEY = os.environ.get(LOCATION_KEY);
+
 console.log('Hello, World!');
 
 const state = {
@@ -19,26 +22,7 @@ const changeTempColor = () => {
   }
 };
 
-const changeTemp = () => {
-  const currentTemp = document.getElementById('display-temp');
-  const arrowUpButton = document.getElementById('arrow-up');
-  const arrowDownButton = document.getElementById('arrow-down');
-  arrowUpButton.addEventListener('click', () => {
-    state.temp += 1;
-    currentTemp.textContent = state.temp;
-    changeTempColor();
-    changeLandscape();
-  });
-  arrowDownButton.addEventListener('click', () => {
-    state.temp -= 1;
-    currentTemp.textContent = state.temp;
-    changeTempColor();
-    changeLandscape();
-  });
-};
-
-// Logic to change landscape based on temperature input
-
+// Change landscape based on temperature input
 const changeLandscape = () => {
   const currentTemp = document.getElementById('display-temp');
   const currentLandscape = document.getElementById('landscape-container');
@@ -46,7 +30,6 @@ const changeLandscape = () => {
   const hotDog = document.getElementById('hot-dog');
   const autumnDog = document.getElementById('autumn-dog');
   const springDog = document.getElementById('spring-dog');
-  // currentLandscape.style.display = 'none';
   if (state.temp >= 80) {
     hotDog.style.display = 'block';
     springDog.style.display = 'none';
@@ -70,40 +53,88 @@ const changeLandscape = () => {
   }
 };
 
+const changeTemp = () => {
+  const currentTemp = document.getElementById('display-temp');
+  const arrowUpButton = document.getElementById('arrow-up');
+  const arrowDownButton = document.getElementById('arrow-down');
+  arrowUpButton.addEventListener('click', () => {
+    state.temp += 1;
+    currentTemp.textContent = state.temp;
+    changeTempColor();
+    changeLandscape();
+  });
+  arrowDownButton.addEventListener('click', () => {
+    state.temp -= 1;
+    currentTemp.textContent = state.temp;
+    changeTempColor();
+    changeLandscape();
+  });
+};
+
 const changeCityName = () => {
-  const currentCity = document.querySelector("#current-city");
-  const cityInput = document.querySelector("#input-city");
-  const resetButton = document.querySelector(".reset-button");
+  const currentCity = document.querySelector('#current-city');
+  const cityInput = document.querySelector('#input-city');
+  const resetButton = document.querySelector('.reset-button');
   cityInput.addEventListener('input', (e) => {
     currentCity.textContent = e.target.value;
-  })
+  });
   resetButton.addEventListener('click', () => {
-    currentCity.textContent = "Seattle, WA";
-    cityInput.value = ""
-  })
-}; 
+    currentCity.textContent = 'Seattle, WA';
+    cityInput.value = '';
+  });
+};
+
+// Get weather! We should add a <button> element that when clicked updates
+// and displays the realtime temperature of the currently displayed city name.
+
+// getLatLon should:
+// take the current input from the box
+// use it to make a GET request with Location IQ
+// return lat, long
+
+// getWeather should:
+// add event listener on click, event is getWeather function
+// call getLatLon
+// use the return value make GET request for weather at those coordinates
+// return temperature to state variable for temp
+
+const getLatLon = () => {
+  const cityInput = document.querySelector('#input-city');
+  let latitude, longitude;
+  axios
+    .get('https://us1.locationiq.com/v1/search.php', {
+      params: {
+        key: LOCATIONIQ_KEY,
+        city: cityInput,
+        format: 'json',
+      },
+    })
+    .then((response) => {
+      latitude = response.data[0].lat;
+      longitude = response.data[0].lon;
+      console.log('successful getWeather request', latitude, longitude);
+      return [latitude, longitude];
+    })
+    .catch((error) => {
+      console.log("that city doesn't exist");
+    });
+};
 
 const changeSky = () => {
-  const currentSky = document.querySelector("#sky-select");
+  const currentSky = document.querySelector('#sky-select');
   const backgroundColor = document.querySelector('body');
   currentSky.addEventListener('change', () => {
-    if (currentSky.value === "sunny") {
-      backgroundColor.style.backgroundColor = 'goldenrod'
-    } else if (currentSky.value === "cloudy") {
-      backgroundColor.style.backgroundColor = 'grey'
-    } else if (currentSky.value === "raining") {
-      backgroundColor.style.backgroundColor = 'blue'
-    } else if (currentSky.value === "snowing") {
-      backgroundColor.style.backgroundColor = 'white'
+    if (currentSky.value === 'sunny') {
+      backgroundColor.style.backgroundColor = 'goldenrod';
+    } else if (currentSky.value === 'cloudy') {
+      backgroundColor.style.backgroundColor = 'grey';
+    } else if (currentSky.value === 'raining') {
+      backgroundColor.style.backgroundColor = 'blue';
+    } else if (currentSky.value === 'snowing') {
+      backgroundColor.style.backgroundColor = 'white';
     }
-  
-  })
-
-}
-
-
-
-
+  });
+};
 
 // function calls for all defined functions
 document.addEventListener('DOMContentLoaded', changeTemp);
