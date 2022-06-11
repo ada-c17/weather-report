fe110cc174e3899c5704ccaa8ef97dbd399e1f6f
Nikki Torab <nikkitorab@gmail.com>
2022-06-11 16:39:54 -0700

    changing city calls location/weather apis and updates temperature

--- src/index.js
+++ src/index.js
@@ -1,52 +1,58 @@
 'use strict';
-const axios = require('axios');
+// const axios = require('axios');
 
 // changing color of temperature value
 const tempColor = (temp) => {
-	if (temp >= 80) {
-		document.getElementById('temp-value').style.color = "red";
+  if (temp >= 80) {
+    document.getElementById('temp-value').style.color = 'red';
   } else if (temp >= 70 && temp <= 79) {
-		document.getElementById('temp-value').style.color = "orange";
-	} else if (temp >= 60 && temp <= 69) {
-		document.getElementById('temp-value').style.color = "yellow";
-	} else if (temp >= 50 && temp <= 59) {
-		document.getElementById('temp-value').style.color = "green";
-	} else {
-	document.getElementById('temp-value').style.color = "teal";
-	}
-}
-
+    document.getElementById('temp-value').style.color = 'orange';
+  } else if (temp >= 60 && temp <= 69) {
+    document.getElementById('temp-value').style.color = 'yellow';
+  } else if (temp >= 50 && temp <= 59) {
+    document.getElementById('temp-value').style.color = 'green';
+  } else {
+    document.getElementById('temp-value').style.color = 'teal';
+  }
+};
 
 // changing landscape emojis
 const landscapeChange = (temp) => {
-	if (temp >= 80) {
-		document.getElementById('landscape').textContent = `"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"`;
+  if (temp >= 80) {
+    document.getElementById(
+      'landscape'
+    ).textContent = `"🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"`;
   } else if (temp >= 70 && temp <= 79) {
-		document.getElementById('landscape').textContent = `"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"`;
-	} else if (temp >= 60 && temp <= 69) {
-		document.getElementById('landscape').textContent = `"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"`; 
-	} else {
-		document.getElementById('landscape').textContent = `"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"`;
-	}
-}
+    document.getElementById(
+      'landscape'
+    ).textContent = `"🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"`;
+  } else if (temp >= 60 && temp <= 69) {
+    document.getElementById(
+      'landscape'
+    ).textContent = `"🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"`;
+  } else {
+    document.getElementById(
+      'landscape'
+    ).textContent = `"🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"`;
+  }
+};
 
 // increasing/decreasing temperature value
 const increaseTemp = () => {
   let temp = parseInt(document.getElementById('temp-value').textContent);
   temp++;
   document.getElementById('temp-value').textContent = temp;
-	tempColor(temp);
-	landscapeChange(temp);
+  tempColor(temp);
+  landscapeChange(temp);
 };
 
 const decreaseTemp = () => {
   let temp = parseInt(document.getElementById('temp-value').textContent);
   temp--;
   document.getElementById('temp-value').textContent = temp;
-	tempColor(temp);
-	landscapeChange(temp);
+  tempColor(temp);
+  landscapeChange(temp);
 };
-
 
 const upEvent = () => {
   const upButton = document.getElementById('up-button');
@@ -62,22 +68,53 @@ const downEvent = () => {
 const cityInput = document.getElementById('city-input');
 const cityName = document.getElementById('city-name');
 
-const updateValue = (cityInput) => {
-	cityName.textContent = cityInput.target.value;
-}
+const updateCityValue = (cityInput) => {
+  cityName.textContent = cityInput.target.value;
 
-// proxy server GET for locationIQ
-axios
-	.get(`http://127.0.0.1:5000/location?q=${cityName.textContent}`)
-	.then((response) => {
-		console.log(response.data[0].lat, response.data[0].lon);
-	}).catch((error) => {
-		console.log('This is an error');
-	})
+  findLatitudeAndLongitude();
+};
 
+const findLatitudeAndLongitude = () => {
+  let latitude, longitude;
+  axios
+    .get(`http://127.0.0.1:5000/location?q=${cityName.textContent}`)
+    .then((response) => {
+      latitude = response.data[0].lat;
+      longitude = response.data[0].lon;
+      console.log('found latitude and longitude');
 
+      getWeather(latitude, longitude);
+    })
+    .catch((error) => {
+      console.log('error in findLatitudeAndLongitude');
+    });
+};
 
+const getWeather = (latitude, longitude) => {
+  axios
+    .get(`http://127.0.0.1:5000/weather?lat=${latitude}&lon=${longitude}`)
 
-cityInput.addEventListener('change', updateValue);
+    .then((response) => {
+      const tempKelvin = response.data.current.temp;
+
+      const temp = Math.floor(1.8 * (tempKelvin - 273) + 32);
+
+      const tempVal = document.getElementById('temp-value');
+      tempVal.textContent = temp;
+
+      return temp;
+    });
+};
+
+// const updateTemp = () => {
+//   let tempVal = document.getElementById('temp-value');
+
+//   findLatitudeAndLongitude();
+
+// };
+
+document.addEventListener('change', updateCityValue);
+// document.addEventListener('change', updateTemp);
+
 document.addEventListener('DOMContentLoaded', upEvent);
 document.addEventListener('DOMContentLoaded', downEvent);
--- src/test.js
+++ src/test.js
@@ -1,11 +1,120 @@
 const axios = require('axios');
 
-const locQuery = "Seattle";
+// const locQuery = "Seattle";
 
-axios
-	.get(`http://127.0.0.1:5000/location?q=${locQuery}`)
-	.then((response) => {
-		console.log(response.data[0].lat, response.data[0].lon);
-	}).catch((error) => {
-		console.log('This is an error');
-	})
+// axios
+// 	.get(`http://127.0.0.1:5000/location?q=${locQuery}`)
+// 	.then((response) => {
+// 		console.log(response.data[0].lat, response.data[0].lon);
+// 	}).catch((error) => {
+// 		console.log('This is an error');
+// 	})
+
+// const findLatitudeAndLongitude = () => {
+//   let latitude, longitude;
+//   axios
+//     .get(`http://127.0.0.1:5000/location?q=${cityName.textContent}`)
+//     .then((response) => {
+//       latitude = response.data[0].lat;
+//       longitude = response.data[0].lon;
+//       console.log('found latitude and longitude');
+//       getWeather(latitude, longitude);
+//     })
+//     .catch((error) => {
+//       console.log('error in findLatitudeAndLongitude');
+//     });
+// };
+
+// const getWeather = (latitude, longitude) => {
+//   axios
+//     .get(`http://127.0.0.1:5000/weather?lat=${latitude}&lon=${longitude}`)
+
+//     .then((response) => {
+//       tempKelvin = response.current.temp;
+
+//       temp = Math.floor(1.8 * (tempKelvin - 273) + 32);
+
+//       console.log(temp);
+//     });
+// };
+
+//************************* */
+// const findLatitudeAndLongitude = (city) => {
+//   let latitude, longitude;
+//   axios
+//     .get(`http://127.0.0.1:5000/location?q=${city}`)
+//     .then((response) => {
+//       latitude = response.data[0].lat;
+//       longitude = response.data[0].lon;
+//       console.log('found latitude and longitude');
+
+//       getWeather(latitude, longitude);
+//     })
+//     .catch((error) => {
+//       console.log('error in findLatitudeAndLongitude');
+//     });
+// };
+
+// const getWeather = (latitude, longitude) => {
+//   axios
+//     .get(`http://127.0.0.1:5000/weather?lat=${latitude}&lon=${longitude}`)
+
+//     .then((response) => {
+//       const tempKelvin = response.data.current.temp;
+
+//       const temp = Math.floor(1.8 * (tempKelvin - 273) + 32);
+
+//       console.log(temp);
+
+//       return temp;
+//     });
+// };
+
+// const city = 'Seattle';
+// findLatitudeAndLongitude(city);
+
+//************************* */
+//********************* */
+let z = [];
+
+const findLatitudeAndLongitude = (city) => {
+  let latitude, longitude;
+  axios
+    .get(`http://127.0.0.1:5000/location?q=${city}`)
+    .then((response) => {
+      latitude = response.data[0].lat;
+      longitude = response.data[0].lon;
+      console.log('found latitude and longitude');
+      console.log('1');
+
+      getWeather(latitude, longitude);
+      console.log(z);
+    })
+    .catch((error) => {
+      console.log('error in findLatitudeAndLongitude');
+    });
+};
+
+const getWeather = (latitude, longitude) => {
+  axios
+    .get(`http://127.0.0.1:5000/weather?lat=${latitude}&lon=${longitude}`)
+
+    .then((response) => {
+      const tempKelvin = response.data.current['temp'];
+
+      const tempFar = Math.floor(1.8 * (tempKelvin - 273) + 32);
+
+      console.log('2');
+
+      z.push(tempFar);
+      console.log(z);
+
+      return tempFar;
+    });
+};
+
+const xx = () => {
+  console.log(z);
+};
+// console.log(findLatitudeAndLongitude('Seattle'));
+findLatitudeAndLongitude('Seattle');
