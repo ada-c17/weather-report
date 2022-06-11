863546fa17cd3e55cb67c0c59cc6547490fc3609
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-06-10 21:56:12 -0500

    Begin to add increase/decrease temperature event handling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 'use strict';
 
 const axios = require('axios');
+
+const state = {
+  temperature: 0,
+};
+
+// increase and decrease temperature
+const increaseTemperature = () => {
+  state.temperature += 1;
+  const temperatureContainer = document.getElementById('temperature');
+
+  temperatureContainer.textContent = `${state.temperature}°`;
+};
+
+const decreaseTemperature = () => {
+  state.temperature -= 1;
+  const temperatureContainer = document.getElementById('temperature');
+
+  temperatureContainer.textContent = `${state.temperature}°`;
+};
+
+const registerEventHandlers = () => {
+  const increaseTempButton = document.getElementById('increase-temp');
+  increaseTempButton.addEventListener('click', increaseTemperature);
+
+  const decreaseTempButton = document.getElementById('decrease-temp');
+  decreaseTempButton.addEventListener('click', decreaseTemperature);
+};
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
