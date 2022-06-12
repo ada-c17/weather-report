e37982e0ebd9e646a785afc1f58c7c97a15604f3
Tyrah <tyrah96@gmail.com>
2022-06-12 15:19:57 -0700

    add drop down for sky selector

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//temperature
+//temperature====================================
 
 //temperature starting state
 const state = {
@@ -8,14 +8,26 @@ const state = {
 //increase temperature behavior
 const addTemp = () => {
   state.currentTemp += 1;
-  const currentTempContainer = document.querySelector('#currentTemp');
+  const currentTempContainer = document.querySelector('.currentTemp');
   currentTempContainer.textcontent = `${state.currentTemp}`;
 };
 
-//register event
+//decrease temperature behavior
+const subtractTemp = () => {
+  state.currentTemp -= 1;
+  const currentTempContainer = document.querySelector('.currentTemp');
+  currentTempContainer.textcontent = `${state.currentTemp}`;
+};
+
+//register events
 const registerEventHandlers = () => {
+  //retrive from doc
   const upArrow = document.querySelector('.up-arrow');
+  const downArrow = document.querySelector('.down-arrow');
+
+  //attach event
   upArrow.addEventListener('click', addTemp);
+  downArrow.addEventListener('click', subtractTemp);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
