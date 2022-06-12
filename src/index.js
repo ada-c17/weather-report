02e9425b4869cd7a508965f76c2c118c49a0d10e
shelby <faulconershelby@gmail.com>
2022-06-11 20:39:03 -0700

    adjusted index for correct file for js and wrote js for temp up and temp down buttons

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
 "use strict";
 
-const axios = require('axios');
+// const axios = require('axios');
 
+const state = {
+    temp: 00,
+};
+
+const addTemp = () => {
+    state.temp += 1;
+    const tempCount = document.querySelector("#temp");
+    tempCount.textContent = `${state.temp}`;
+};
+
+const lowerTemp = () => {
+    state.temp -= 1;
+    const tempCount = document.querySelector("#temp");
+    tempCount.textContent = `${state.temp}`;
+};
+
+const registerEventHandlers = () => {
+    const tempUpButton = document.querySelector("#raiseTemp");
+    tempUpButton.addEventListener("click", addTemp);
+
+    const tempDownButton = document.querySelector("#lowerTemp");
+    tempDownButton.addEventListener("click", lowerTemp);
+};
+
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
