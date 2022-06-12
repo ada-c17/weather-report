e4dd8d567d28f3db2dc05356b9ac428d26b49467
Nina <ninagetsbored@gmail.com>
2022-06-12 13:49:04 -0700

    completed wave 04

--- src/index.js
+++ src/index.js
@@ -51,12 +51,33 @@ const state = {
     displayCity.textContent = state.city;
   };
 
-  const registerEventHandlers = () => {
-    const temperatureUp = document.getElementById('up_temperature');
-    temperatureUp.addEventListener('click', increaseTemperature);
+  const getRealtimeTemperature = async () => {
+    const locationData = await axios.get(
+      `http://127.0.0.1:5000/location?q=${state.city}`);
+    const lat = locationData.data[0].lat;
+    const lon = locationData.data[0].lon;
+    getWeatherFromLocation(lat, lon);
+  };
 
-    const temperatureDown = document.getElementById('down_temperature');
-    temperatureDown.addEventListener('click', decreaseTemperature);
+  const getWeatherFromLocation = async (lat, lon) => {
+    const weatherData = await axios.get(
+      `http://127.0.0.1:5000/weather?lat=${lat}&lon=${lon}`
+    );
+    const temperatureKelvin = weatherData.data.current.temp;
+    state.temperature = Math.floor(1.8 * (temperatureKelvin - 273.15) + 32)
+    displayTemperature();
+    changeTemperatureDecor();
+  };
+
+  const registerEventHandlers = () => {
+    const upTemperature = document.getElementById('up_temperature');
+    upTemperature.addEventListener('click', increaseTemperature);
+
+    const downTemperature = document.getElementById('down_temperature');
+    downTemperature.addEventListener('click', decreaseTemperature);
+
+    const realtimeTemperature = document.getElementById('realtime_temperature');
+    realtimeTemperature.addEventListener('click', getRealtimeTemperature);
 
     inputCity.addEventListener('input', changeCity);
   };
