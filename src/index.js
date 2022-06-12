0649927f1d4cb7011c10eceeaf386ca146322a47
Lin Liu <lin_6066@yahoo.com>
2022-06-11 21:08:34 -0700

    refactor functions, complete all waves

--- src/index.js
+++ src/index.js
@@ -1,160 +1,215 @@
-const dropBar = document.querySelector('#sky-selector');
-const getSelectedSky = (backgrounds) => {
-  const sky = dropBar.options[dropBar.selectedIndex].text;
-  let background;
-  switch (true) {
-    default:
-      background = backgrounds[0];
-      break;
-    case sky === 'Sunny':
-      background = backgrounds[1];
-      break;
-    case sky === 'Cloudy':
-      background = backgrounds[2];
-      break;
-    case sky === 'Rainy':
-      background = backgrounds[3];
-      break;
-    case sky === 'Snowy':
-      background = backgrounds[4];
-      break;
-    case sky === 'Windy':
-      background = backgrounds[5];
+class Helper {
+  static getSelectedSky() {
+    const backgrounds = [
+      './assets/defaultSky.jpeg',
+      './assets/sunny.jpeg',
+      './assets/cloudy.jpeg',
+      './assets/rainy.jpeg',
+      './assets/snowy.jpeg',
+      './assets/windy.jpeg',
+    ];
+    const dropBar = document.querySelector('#sky-selector');
+    const sky = dropBar.options[dropBar.selectedIndex].text;
+    let background;
+    switch (true) {
+      default:
+        background = backgrounds[0];
+        break;
+      case sky === 'Sunny':
+        background = backgrounds[1];
+        break;
+      case sky === 'Cloudy':
+        background = backgrounds[2];
+        break;
+      case sky === 'Rainy':
+        background = backgrounds[3];
+        break;
+      case sky === 'Snowy':
+        background = backgrounds[4];
+        break;
+      case sky === 'Windy':
+        background = backgrounds[5];
+    }
+    return background;
   }
-  return background;
-};
 
-const skyBackground = document.querySelector('body');
-const changeSkyBackground = () => {
-  const imgs = [
-    '/assets/defaultSky.jpeg',
-    '/assets/sunny.jpeg',
-    '/assets/cloudy.jpeg',
-    '/assets/rainy.jpeg',
-    '/assets/snowy.jpeg',
-    '/assets/windy.jpeg',
-  ];
-  const img = getSelectedSky(imgs);
-  skyBackground.style.backgroundImage = `url(${img})`;
-};
-
-skyBackground.addEventListener('change', () => {
-  changeSkyBackground();
-});
-
-const temperature = document.querySelector('#temp-value');
-const getEffectByTempRange = (effects) => {
-  const value = parseInt(temperature.innerText);
-  let effect;
-  switch (true) {
-    case value <= 59:
-      effect = effects[0];
-      break;
-    case value <= 69:
-      effect = effects[1];
-      break;
-    case value <= 79:
-      effect = effects[2];
-      break;
-    default:
-      effect = effects[3];
+  static getEffectByTempValue(tempValue, effects) {
+    let effect;
+    switch (true) {
+      case tempValue <= 59:
+        effect = effects[0];
+        break;
+      case tempValue <= 69:
+        effect = effects[1];
+        break;
+      case tempValue <= 79:
+        effect = effects[2];
+        break;
+      default:
+        effect = effects[3];
+    }
+    return effect;
   }
-  return effect;
-};
 
-const changeTempTextColor = () => {
-  const colors = ['green', 'yellow', 'orange', 'red'];
-  const color = getEffectByTempRange(colors);
-  temperature.style.color = color;
-};
-
-const landscape = document.querySelector('#temp-display');
-const changeLandscape = () => {
-  const imgs = [
-    './assets/winter.jpg',
-    './assets/fall.jpg',
-    './assets/spring.jpg',
-    './assets/summer.jpg',
-  ];
-  const img = getEffectByTempRange(imgs);
-  landscape.style.backgroundImage = `url(${img})`;
-};
-
-const decreaseButton = document.querySelector('#temp-down');
-decreaseButton.addEventListener('click', () => {
-  const value = parseInt(temperature.innerText);
-  temperature.innerText = value - 1;
-  changeTempTextColor();
-  changeLandscape();
-});
-
-const increaseButton = document.querySelector('#temp-up');
-increaseButton.addEventListener('click', () => {
-  const value = parseInt(temperature.innerText);
-  temperature.innerText = value + 1;
-  changeTempTextColor();
-  changeLandscape();
-});
-
-const cityName = document.querySelector('#temp-display h1');
-const changeCityName = (city) => {
-  if (city == '') {
-    city = 'seattle';
+  static getTempEffect(tempValue) {
+    const colors = ['green', 'yellow', 'orange', 'red'];
+    const landscapes = [
+      './assets/winter.jpg',
+      './assets/fall.jpg',
+      './assets/spring.jpg',
+      './assets/summer.jpg',
+    ];
+    const color = this.getEffectByTempValue(tempValue, colors);
+    const img = this.getEffectByTempValue(tempValue, landscapes);
+    return { color: color, img: img };
   }
-  cityName.innerText = city;
-};
 
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const city = form.elements.query.value;
-  changeCityName(city);
-});
-
-const resetCityButton = document.querySelector('#reset-city');
-resetCityButton.addEventListener('click', () => {
-  changeCityName('Seattle');
-  form.elements.query.value = '';
-});
-
-const convertTempKtoF = (tempK) => {
-  return Math.round(1.8 * (tempK - 273) + 32);
-};
-
-const getTempButton = document.querySelector('#get-temp');
-getTempButton.addEventListener('click', () => {
-  const city = form.elements.query.value;
-  updateTemp(city);
-  changeCityName(city);
-  const value = parseInt(temperature.innerText);
-  changeTempTextColor();
-  changeLandscape();
-});
-
-const updateTemp = (city) => {
-  if (city == '') {
-    city = 'seattle';
+  static changeCityName(city = 'Seattle') {
+    const cityName = document.querySelector('#temp-display h1');
+    cityName.innerText = city;
   }
-  axios
-    .get(`http://127.0.0.1:5000/location?q=${city}`)
-    .then((response) => {
-      lat = response.data[0].lat;
-      lon = response.data[0].lon;
+
+  static convertTempKtoF(tempK) {
+    return Math.round(1.8 * (tempK - 273) + 32);
+  }
+
+  // API call with async function
+  static getLocation = async (city) => {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:5000/location?q=${city}`
+      );
+      const lat = response.data[0].lat;
+      const lon = response.data[0].lon;
       return [lat, lon];
-    })
-    .then((response) => {
-      axios
-        .get(
-          `http://127.0.0.1:5000/weather?lat=${response[0]}&lon=${response[1]}`
-        )
-        .then((response) => {
-          const tempValue = document.querySelector('#temp-value');
-          kTemp = response.data.current.temp;
-          FTemp = convertTempKtoF(kTemp);
-          tempValue.innerText = FTemp;
-        });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+    } catch (e) {
+      console.log('getLocation Error!');
+      console.log(e);
+    }
+  };
+
+  static getWeather = async (city) => {
+    try {
+      const location = await this.getLocation(city);
+      const response = await axios.get(
+        `http://127.0.0.1:5000/weather?lat=${location[0]}&lon=${location[1]}`
+      );
+      const temp = this.convertTempKtoF(response.data.current.temp);
+      return temp;
+    } catch (e) {
+      console.log('getWeather Error!');
+      console.log(e);
+    }
+  };
+
+  static updateTemps = async (city = 'Seattle') => {
+    try {
+      const temp = await this.getWeather(city);
+      const tempValue = document.querySelector('#temp-value');
+      tempValue.innerText = temp;
+    } catch (e) {
+      console.log('updateTemp Error!');
+      console.log(e);
+    }
+  };
+
+  //API call in chain
+  // const updateTemp = (city) => {
+  //   if (city == '') {
+  //     city = 'seattle';
+  //   }
+  //   axios
+  //     .get(`http://127.0.0.1:5000/location?q=${city}`)
+  //     .then((response) => {
+  //       lat = response.data[0].lat;
+  //       lon = response.data[0].lon;
+  //       return [lat, lon];
+  //     })
+  //     .then((response) => {
+  //       axios
+  //         .get(
+  //           `http://127.0.0.1:5000/weather?lat=${response[0]}&lon=${response[1]}`
+  //         )
+  //         .then((response) => {
+  //           const tempValue = document.querySelector('#temp-value');
+  //           kTemp = response.data.current.temp;
+  //           FTemp = convertTempKtoF(kTemp);
+  //           tempValue.innerText = FTemp;
+  //         });
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //     });
+  // };
+}
+
+const registerEventHandlers = () => {
+  // temperature value need to be udpated either manually or real time
+  const temperature = document.querySelector('#temp-value');
+  // landscape image will be updated base on the temperature value
+  const landscape = document.querySelector('#temp-display');
+
+  // update body background image base on sky selection
+  const skyBackground = document.querySelector('body');
+  skyBackground.addEventListener('change', () => {
+    const img = Helper.getSelectedSky();
+    skyBackground.style.backgroundImage = `url(${img})`;
+  });
+
+  const decreaseButton = document.querySelector('#temp-down');
+  decreaseButton.addEventListener('click', () => {
+    const tempValue = parseInt(temperature.innerText) - 1;
+    temperature.innerText = tempValue;
+
+    const color = Helper.getTempEffect(tempValue).color;
+    temperature.style.color = color;
+
+    const img = Helper.getTempEffect(tempValue).img;
+    landscape.style.backgroundImage = `url(${img})`;
+  });
+
+  const increaseButton = document.querySelector('#temp-up');
+  increaseButton.addEventListener('click', () => {
+    const tempValue = parseInt(temperature.innerText) + 1;
+    temperature.innerText = tempValue;
+
+    const color = Helper.getTempEffect(tempValue).color;
+    temperature.style.color = color;
+
+    const img = Helper.getTempEffect(tempValue).img;
+    landscape.style.backgroundImage = `url(${img})`;
+  });
+
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = form.elements.query.value;
+    Helper.changeCityName(city);
+  });
+
+  const resetCityButton = document.querySelector('#reset');
+  resetCityButton.addEventListener('click', () => {
+    Helper.changeCityName();
+    Helper.updateTemps();
+    form.elements.query.value = '';
+  });
+
+  const getTempButton = document.querySelector('#get-temp');
+  getTempButton.addEventListener('click', () => {
+    let city = form.elements.query.value;
+    if (city == '') {
+      city = 'Seattle';
+    }
+    Helper.updateTemps(city);
+    Helper.changeCityName(city);
+
+    const tempValue = parseInt(temperature.innerText);
+    const color = Helper.getTempEffect(tempValue).color;
+    temperature.style.color = color;
+
+    const img = Helper.getTempEffect(tempValue).img;
+    landscape.style.backgroundImage = `url(${img})`;
+  });
 };
+
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
