42be3d0d21cadf4bb339a4e61a5873b83ac5ad55
shelby <faulconershelby@gmail.com>
2022-06-12 12:09:58 -0700

    checking something

--- src/index.js
+++ src/index.js
@@ -18,6 +18,33 @@ const lowerTemp = () => {
     tempCount.textContent = `${state.temp}`;
 };
 
+const tempThemeToggle = () => {
+    addTemp();
+    lowerTemp();
+
+    const tempAdjustElement = document.getElementsByClassName("tempAdjust");
+    const tempElement = document.getElementById("temp");
+
+    if (state.temp >= 32){
+        // change background, #tempAdjust,
+        tempAdjustElement.textContent = "tooHot";
+        
+        // and #tempAdjust h2 color -> too hot
+    }
+    else if (state.temp >= 25 && state.temp < 32){
+        // change background, #tempAdjust,
+        // and #tempAdjust h2 color -> hot
+    }
+    else if (state.temp > 0 && state.temp <= 11) {
+        // change background, #tempAdjust,
+        // and #tempAdjust h2 color -> cold
+    }
+    else if (state.temp <= 0){
+        // change background, #tempAdjust,
+        // and #tempAdjust h2 color -> freezing
+    }
+}
+
 const registerEventHandlers = () => {
     const tempUpButton = document.querySelector("#raiseTemp");
     tempUpButton.addEventListener("click", addTemp);
