dd4144823d17b120fd2556f38f739921bec77201
Philomena Kelly <philomena.a.kelly@outlook.com>
2022-06-12 22:58:38 -0400

    added functionality to change temp and update color changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const updatecity = (event) => {
+const state = {
+  city: 'new milford, ct',
+  temperature: 0,
+};
+
+const updateCity = (event) => {
   city = document.getElementById('city').value;
-  console.log(city);
 
   let searchparams = {
     q: city,
@@ -12,52 +16,77 @@ const updatecity = (event) => {
     .get('http://127.0.0.1:5000/location', { params: searchparams })
 
     .then((response) => {
-      console.log('success!', response.data[0]['lat'], response.data[0]['lon']);
-      document.getElementById(
-        'latlon'
-      ).textContent += ` success! ${response.data[0]['lat']}, ${response.data[0]['lon']}`;
-
-      axios
-        .get('http://127.0.0.1:5000/weather', {
-          params: {
-            lat: response.data[0]['lat'],
-            lon: response.data[0]['lon'],
-          },
-        })
-
-        .then((response) => {
-          console.log('success!', response.data.current.temp);
-          document.getElementById(
-            'weather'
-          ).textContent += `success! ${response.data.current.temp}`;
-        })
-        .catch((error) => {
-          console.log('error!', error.response.data);
-        });
+      getWeather(response.data[0]['lat'], response.data[0]['lon']);
     })
     .catch((error) => {
       console.log('error!', error.response.data);
     });
 };
 
-const getWeather = (lat, lon) => {
+const getWeather = (lattitude, longitude) => {
   axios
-    .get('http://127.0.0.1:5000/weather', { params: { lat: lat, lon: lon } })
+    .get('http://127.0.0.1:5000/weather', {
+      params: {
+        lat: lattitude,
+        lon: longitude,
+      },
+    })
 
     .then((response) => {
-      console.log('success!', response.data[0]['current']['temp']);
-      document.getElementById(
-        'weather'
-      ).textContent += `success! ${response.data}`;
+      console.log('success!', response.data.current.temp);
+      kelvinToF(response.data.current.temp);
+      updateTempAndColor();
     })
     .catch((error) => {
       console.log('error!', error.response.data);
     });
 };
+
+const raiseTemp = (event) => {
+  state.temperature += 1;
+  updateTempAndColor();
+};
+
+const lowerTemp = (event) => {
+  state.temperature -= 1;
+  updateTempAndColor();
+};
+
+const kelvinToF = (tempInK) => {
+  state.temperature = Math.round(1.8 * (tempInK - 273) + 32);
+};
+
+const calculateColor = () => {
+  let colorPercent;
+  if (state.temperature > 110) {
+    colorPercent = (110 + 10) / 120;
+  } else if (state.temperature < -10) {
+    colorPercent = (-10 + 10) / 120;
+  } else {
+    colorPercent = (state.temperature + 10) / 120;
+  }
+  let colorRotate = (100 - colorPercent) * 240;
+  return Math.round(colorRotate);
+};
+
+const updateTempAndColor = () => {
+  document.getElementById(
+    'weather'
+  ).style.color = `hsl(${calculateColor()}, 75%, 50%)`;
+  document.getElementById('weather').textContent = state.temperature;
+};
+
+updateTempAndColor();
 
 const registerEventHandlers = (event) => {
   const cityField = document.querySelector('#cityButton');
-  cityField.addEventListener('click', updatecity);
+  const warmer = document.querySelector('#warmer');
+  const colder = document.querySelector('#colder');
+
+  cityField.addEventListener('click', updateCity);
+
+  warmer.addEventListener('click', raiseTemp);
+  colder.addEventListener('click', lowerTemp);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
