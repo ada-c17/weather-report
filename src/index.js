ea713b45330f773a57b1f32f21ef0d20592e82af
Tyrah <tyrah96@gmail.com>
2022-06-12 18:51:43 -0700

    finished wave 2

--- src/index.js
+++ src/index.js
@@ -8,22 +8,53 @@ const state = {
 //increase temperature behavior
 const addTemp = () => {
   state.currentTemp += 1;
-  const currentTempContainer = document.querySelector('.currentTemp');
-  currentTempContainer.textcontent = `${state.currentTemp}`;
+  let currentTempContainer = document.querySelector('#currentTemp');
+  currentTempContainer.innerHTML = `${state.currentTemp}`;
+  changeTempColor();
 };
 
 //decrease temperature behavior
 const subtractTemp = () => {
   state.currentTemp -= 1;
-  const currentTempContainer = document.querySelector('.currentTemp');
-  currentTempContainer.textcontent = `${state.currentTemp}`;
+  let currentTempContainer = document.querySelector('#currentTemp');
+  currentTempContainer.innerHTML = `${state.currentTemp}`;
+  changeTempColor();
+};
+//change temperature color
+const changeTempColor = () => {
+  if (state.currentTemp >= 80) {
+    document.querySelector('.temperature').style.background = 'red';
+    changeLandScape('#hot');
+  } else if (state.currentTemp >= 70 && state.currentTemp <= 79) {
+    document.querySelector('.temperature').style.background = 'orange';
+    changeLandScape('#warm');
+  } else if (state.currentTemp >= 60 && state.currentTemp <= 69) {
+    document.querySelector('.temperature').style.background = 'yellow';
+    changeLandScape('#windy');
+  } else if (state.currentTemp >= 50 && state.currentTemp <= 59) {
+    document.querySelector('.temperature').style.background = 'green';
+    changeLandScape('#cold');
+  } else {
+    document.querySelector('.temperature').style.background = 'Teal';
+    changeLandScape('#cold');
+  }
+};
+
+//change landscape
+const changeLandScape = (query) => {
+  const weatherChildren = document.querySelector('.weather').children;
+
+  for (let child = 0; child < weatherChildren.length; child++) {
+    weatherChildren[child].style.display = 'none';
+  }
+  document.querySelector(query).style.display = 'inline';
 };
 
 //register events
 const registerEventHandlers = () => {
   //retrive from doc
-  const upArrow = document.querySelector('.up-arrow');
-  const downArrow = document.querySelector('.down-arrow');
+  const upArrow = document.querySelector('#upArrow');
+  const downArrow = document.querySelector('#downArrow');
 
   //attach event
   upArrow.addEventListener('click', addTemp);
