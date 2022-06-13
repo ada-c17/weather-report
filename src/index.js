0693cd96083fbc5ce776a5048c1fc46ec7adae05
xiomara <xrodri98@gmail.com>
2022-06-13 12:04:03 -0400

    trying to get change sky portion working

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-// function rn(from, to) {
-//   return ~~(Math.random() * (to - from + 1)) + from;
-// }
-
-// function rs() {
-//   return arguments[rn(1, arguments.length) - 1];
-// }
-
-// function boxShadows(max) {
-//   let ret = [];
-//   for (let i = 0; i < max; ++i) {
-//     ret.push(`
-//     ${rn(1, 100)}vw ${rn(1, 100)}vh ${rn(20, 40)}vmin ${rn(1, 20)}vmin
-//     ${rs('#11cbd7', '#c6f1e7', '#f0fff3', '#fa4659')}
-//     `);
-//   }
-//   return ret.join(',');
-// }
-
-// const cloud = document.querySelector('#cloud');
-// function update() {
-//   cloud.style.boxShadow = boxShadows(100);
-// }
-
-// window.addEventListener('load', update);
-// document.addEventListener('click', update);
-
 const state = {
   temp: 75,
+  city: '',
 };
 
 //select elements with get the element by id
@@ -34,10 +8,15 @@ const currentTemp = document.getElementById('currentTemp');
 const incrementButton = document.getElementById('increaseTempButton');
 const decrementButton = document.getElementById('decreaseTempButton');
 const seasonalChange = document.getElementById('seasonalChange');
+const searchCity = document.getElementById('searchCity');
+const cityDisplay = document.getElementById('cityDisplay');
+const currentTempButton = document.getElementById('currentTempButton');
+const selectSkies = document.getElementById('skies').value;
+// const skyChoice = document.getElementById('skyChoice');
 
 const updateTemp = () => {
   currentTemp.textContent = state.temp;
-  // alterTextColor()
+  alterTextColor();
 };
 
 const incrementTemp = () => {
@@ -52,9 +31,42 @@ const decrementTemp = () => {
   alterTextColor();
 };
 
+const updateCity = () => {
+  const currentCity = document.getElementById('searchCity').value;
+  console.log(currentCity);
+  state.city = currentCity;
+  cityDisplay.textContent = state.city;
+};
+
+const changeSky = () => {
+  // let skyContent = document.getElementById('body').style.background
+  //   const skyContent = document.getElementById('skyChoice');
+  //   let skyScape = '';
+  if (selectSkies === 'clearDay') {
+    seasonalChange.textContent = '🔆';
+    // skyContent = 'linear-gradient(to bottom, '#daf4ff', '#d1eaff', '#bfd7ff')'
+  } else if (selectSkies === 'rain') {
+    seasonalChange.textContent = '🌧';
+  } else if (selectSkies === 'wind') {
+    seasonalChange.textContent = '🌬';
+  } else if (selectSkies === 'snow') {
+    seasonalChange.textContent = '❄️';
+  }
+};
+// change the sky
+// const setIcon = (icons, iconID) => {
+//   const skycons = new Skycons({ color: '#2b2d42' });
+//   const currentIcon = icons.replace(/-/g, '_').toUpperCase();
+//   skycons.play();
+//   return skycons.set(iconID, skycons[currentIcon]);
+// };
+
 const registerEventHandlers = () => {
   incrementButton.addEventListener('click', incrementTemp);
   decrementButton.addEventListener('click', decrementTemp);
+  searchCity.addEventListener('input', updateCity);
+  currentTempButton.addEventListener('click', findLatitudeAndLongitude);
+  selectSkies.addEventListener('change', changeSky);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
@@ -62,32 +74,88 @@ document.addEventListener('DOMContentLoaded', registerEventHandlers);
 const alterTextColor = () => {
   if (state.temp > 95) {
     console.log('changing to red');
-    seasonalChange.innerText = '🔥';
+    seasonalChange.textContent = '🔥';
     return (currentTemp.style.color = '#ec5766');
   } else if (state.temp >= 85) {
     console.log('changing to orange');
-    seasonalChange.innerText = '🌊';
+    seasonalChange.textContent = '🌊';
     return (currentTemp.style.color = '#fda187');
   } else if (state.temp >= 75) {
     console.log('changing to yellow');
-    seasonalChange.innerText = '🌴';
-    return (currentTemp.style.color = '#faf59e');
+    seasonalChange.textContent = '🌴';
+    return (currentTemp.style.color = '#F1FFC4');
   } else if (state.temp >= 60) {
-    seasonalChange.innerText = '🌷';
+    seasonalChange.textContent = '🌷';
     console.log('changing to green');
     return (currentTemp.style.color = '#b4f9c0');
   } else if (state.temp >= 45) {
-    seasonalChange.innerText = '🍂';
+    seasonalChange.textContent = '🍂';
     console.log('changing to teal');
     return (currentTemp.style.color = '#5dac97');
   } else if (state.temp >= 30) {
-    seasonalChange.innerText = '🌲';
+    seasonalChange.textContent = '🌲';
     console.log('changing to blue');
-    return (currentTemp.style.color = '#479aff');
+    return (currentTemp.style.color = '#6da3ee');
   } else if (state.temp <= 29) {
-    seasonalChange.innerText = '⛄';
+    seasonalChange.textContent = '⛄';
     console.log('changing to ice blue');
     return (currentTemp.style.color = '#c2f6ff');
   }
   console.log('text color did not change');
+};
+
+// const setIcons = () => {
+//   icons.set('clear-day', Skycons.CLEAR_DAY);
+//   icons.set('rain', Skycons.RAIN);
+//   icons.set('snow', Skycons.SNOW);
+//   icons.set('wind', Skycons.WIND);
+
+//   icons.play();
+// };
+
+const convertKelvinToFahrenheit = (kelvin) => {
+  let fahrenheit;
+  fahrenheit = parseInt(1.8 * (kelvin - 273) + 32);
+  return fahrenheit;
+};
+
+// weather API
+const findLatitudeAndLongitude = () => {
+  let latitude, longitude;
+  axios
+    .get('https://weather-report-proxy-server.herokuapp.com/location', {
+      params: {
+        q: state.city,
+      },
+    })
+    .then((response) => {
+      latitude = response.data[0].lat;
+      longitude = response.data[0].lon;
+
+      // make the next API call here!
+      findTemperature(latitude, longitude);
+    })
+    .catch((error) => {
+      console.log('error in findLatitudeAndLongitude!');
+    });
+};
+
+// const temp = document.getElementById('currentTemp');
+
+const findTemperature = (latitude, longitude) => {
+  axios
+    .get('https://weather-report-proxy-server.herokuapp.com/weather', {
+      params: {
+        lat: latitude,
+        lon: longitude,
+      },
+    })
+    .then((response) => {
+      const kelvin = response.data['current']['temp'];
+      state.temp = convertKelvinToFahrenheit(kelvin);
+      updateTemp();
+    })
+    .catch((error) => {
+      console.log('error in weather info!');
+    });
 };
