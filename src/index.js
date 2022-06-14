9b40b8487dd520b005e0d4c901e6c8a3acdd5b94
Esther Annorzie <estherannorzie@gmail.com>
2022-06-14 11:27:01 -0400

    Clicking up arrow increases temperature

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const state = {
+  temperatureCount: 0,
+};
+
+const increaseTemperatureCount = () => {
+  const displayTemperatureEl = document.getElementById("displayTemperature");
+  state.temperatureCount += 1;
+  displayTemperatureEl.textContent = `Temperature: ${state.temperatureCount}`;
+};
+
+const registerEventHandlers = () => {
+  const upArrowEl = document.getElementById("upArrow");
+  upArrowEl.addEventListener("click", increaseTemperatureCount);
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
