7ab92979b8cd71976d629fc7fa5060c7ac89d3c4
shelby <faulconershelby@gmail.com>
2022-06-13 20:38:39 -0700

    landscape properly changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 // const axios = require('axios');
 
 const state = {
-    temp: 0,
+    temp: 23,
 };
 
 const addTemp = () => {
@@ -19,32 +19,40 @@ const lowerTemp = () => {
 };
 
 const tempThemeToggle = () => {
+    console.log('tempThemeToggle called')
+
     addTemp();
     lowerTemp();
 
-    const tempAdjustElement = document.getElementsByClassName("tempAdjust");
-    const tempElement = document.getElementByClassName("tempBox");
+    const tempAdjustElement = document.getElementById("tempAdjust");
+    // const tempElement = document.getElementById("tempBox");
+    const tempAdjustLandscape = document.getElementById("landScape");
+
+    console.log(tempAdjustElement)
 
     if (state.temp >= 32){
         // change background, #tempAdjust,
         tempAdjustElement.className = "tooHot";
         // and #tempAdjust h2 color -> too hot
-        tempElement.className = "tooHot";
+        // tempElement.className = "tooHot";
+        tempAdjustLandscape.className = "landHot";
     } else if (state.temp >= 25 && state.temp < 32){
         // change background, #tempAdjust,
         tempAdjustElement.className = "hot";
         // and #tempAdjust h2 color -> hot
-        tempElement.className = "hot";
+        // tempElement.className = "hot";
     } else if (state.temp > 0 && state.temp <= 11) {
         // change background, #tempAdjust,
         tempAdjustElement.className = "cold";
         // and #tempAdjust h2 color -> cold
-        tempElement.className = "cold";
+        // tempElement.className = "cold";
+        tempAdjustLandscape.className = "landCold";
     } else if (state.temp <= 0){
         // change background, #tempAdjust,
         tempAdjustElement.className = "freezing";
         // and #tempAdjust h2 color -> freezing
-        tempElement.className = "freezing";
+        // tempElement.className = "freezing";
+        tempAdjustLandscape.className = "landFreezing";
     };
 };
 
