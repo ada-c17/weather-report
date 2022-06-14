c59732fe68fcb01649c83952397eacda2ae16196
Esther Annorzie <estherannorzie@gmail.com>
2022-06-14 19:13:36 -0400

    Displayed temperature color changes based on temperature

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ const state = {
   temperatureCount: 0,
 };
 
-const changeTemperatureColor = () => {
- if (state.temperatureCount >= 80) {
-  
+const changeTemperatureColor = (displayTemperatureEl) => {
+  // if temperature value is 0 put (class)
+  displayTemperatureEl.className = "padding-5";
+  if (state.temperatureCount <= 49) {
+   displayTemperatureEl.classList.add("teal");
+  }
+  else if (state.temperatureCount <= 59) {
+    displayTemperatureEl.classList.add( "green");
+  }
+  else if (state.temperatureCount <= 69) {
+    displayTemperatureEl.classList.add( "yellow");
+  } 
+  else if (state.temperatureCount <= 79) { displayTemperatureEl.classList.add( "orange");
+  }
+  else {
+   displayTemperatureEl.classList.add( "red");
  }
-}
+};
 
 const increaseTemperatureCount = (tempChangeValue) => {
   const displayTemperatureEl = document.getElementById("displayTemperature");
   state.temperatureCount += tempChangeValue;
+  changeTemperatureColor(displayTemperatureEl);
   displayTemperatureEl.textContent = `Temperature: ${state.temperatureCount}`;
 };
 
