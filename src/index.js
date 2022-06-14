08dcf849932c414517658df2f309c7faeefc0a14
Esther Annorzie <estherannorzie@gmail.com>
2022-06-14 11:37:45 -0400

    Clicking the down arrow decreases the temperature

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ const state = {
   temperatureCount: 0,
 };
 
-const increaseTemperatureCount = () => {
+const increaseTemperatureCount = (tempChangeValue) => {
   const displayTemperatureEl = document.getElementById("displayTemperature");
-  state.temperatureCount += 1;
+  state.temperatureCount += tempChangeValue;
   displayTemperatureEl.textContent = `Temperature: ${state.temperatureCount}`;
 };
 
 const registerEventHandlers = () => {
   const upArrowEl = document.getElementById("upArrow");
-  upArrowEl.addEventListener("click", increaseTemperatureCount);
+  upArrowEl.addEventListener("click", () => { increaseTemperatureCount(1)});
+
+  const downArrowEl = document.getElementById("downArrow");
+  downArrowEl.addEventListener("click", () => { increaseTemperatureCount(-1)});
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
