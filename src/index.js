828bec59849e27470c71f57a32842a92b271babe
Kassidy Buslach <kjbuslach@gmail.com>
2022-06-15 12:55:14 -0700

    everything works for the most part

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// import axios from 'axios'
 const state = {
   fahrenheit: 60,
   city: 'Edmonds',
+  sky: 'Sunny',
 };
 let num = state.fahrenheit;
 const tempUp = (event) => {
@@ -21,23 +21,13 @@ const tempDown = (event) => {
   decrease.textContent = `Degrees Fahrenheit:${state.fahrenheit}`;
   return decrease.textContent;
 };
+
 const cityInput = (event) => {
   let cityInput = document.getElementById('input').value;
-  console.log(cityInput);
   state.city = cityInput;
-  let cityHeader = document.querySelector('#city');
+  let cityHeader = document.getElementById('city');
   cityHeader.textContent = `${state.city}`;
-};
-
-const registerEventHandlers = (event) => {
-  const upButton = document.querySelector('#upButton');
-  upButton.addEventListener('click', tempUp);
-
-  const downButton = document.querySelector('#downButton');
-  downButton.addEventListener('click', tempDown);
-
-  //   const changeCity = document.getElementById('enter');
-  //   changeCity.addEventListener(cityInput);
+  getCityWeather();
 };
 
 const tempChangeColor = (num) => {
@@ -45,7 +35,6 @@ const tempChangeColor = (num) => {
   if (state.fahrenheit >= 80) {
     element.style.backgroundColor = 'red';
   } else if (80 > state.fahrenheit && state.fahrenheit >= 70) {
-    console.log('orange');
     element.style.backgroundColor = 'orange';
   } else if (70 > state.fahrenheit && state.fahrenheit >= 60) {
     element.style.backgroundColor = 'yellow';
@@ -76,4 +65,78 @@ const weatherGarden = () => {
   }
 };
 
+const getCityWeather = () => {
+  const q = state.city;
+  axios
+    .get('http://localhost:5000/location', { params: { q } })
+    .then((response) => {
+      console.log('success!', response.data);
+      console.log(response.data[0]['lat']);
+      let lat = response.data[0]['lat'];
+      console.log(lat);
+      let lon = response.data[0]['lon'];
+      axios
+        .get('http://localhost:5000/weather', { params: { lat, lon } })
+        .then((response) => {
+          console.log('success!', response.data);
+          console.log(response.data['current']['temp']);
+          let degrees = response.data['current']['temp'];
+          let degreesF = (degrees - 273) * 1.8 + 32;
+          state.fahrenheit = parseInt(degreesF);
+          updateTemp();
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log('error!', error.response.data);
+        });
+    })
+    .catch((error) => {
+      console.log('error!', error);
+    });
+};
+
+const selectSky = (event) => {
+  const sky = document.getElementById('sky').value;
+  state.sky = sky;
+  const skies = document.getElementById('skies');
+  if (state.sky === 'Sunny') {
+    skies.textContent = '☁️ ☁️ ☁️ ☀️ ☁️ ☁️';
+  } else if (state.sky === 'Cloudy') {
+    skies.textContent = '☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️';
+  } else if (state.sky === 'Rainy') {
+    skies.textContent = '🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧';
+  } else if (state.sky === 'Snowy') {
+    skies.textContent = '🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨';
+  }
+};
+
+const resetCity = (event) => {
+  const resetInput = document.getElementById('input');
+  resetInput.value = 'Edmonds';
+  cityInput();
+  state.fahrenheit = 60;
+  updateTemp();
+};
+
+const updateTemp = () => {
+  const update = document.getElementById('f');
+  update.textContent = `Degrees Fahrenheit:${state.fahrenheit}`;
+};
+const registerEventHandlers = (event) => {
+  const upButton = document.querySelector('#upButton');
+  upButton.addEventListener('click', tempUp);
+
+  const downButton = document.querySelector('#downButton');
+  downButton.addEventListener('click', tempDown);
+
+  const changeCity = document.getElementById('enter');
+  changeCity.addEventListener('click', cityInput);
+
+  selectSky();
+  const changeSky = document.getElementById('sky');
+  changeSky.addEventListener('change', selectSky);
+
+  const resetButton = document.getElementById('reset');
+  resetButton.addEventListener('click', resetCity);
+};
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
