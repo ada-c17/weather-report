45c5831ad33ec8e53ac158a03324a35f21c9208d
Esther Annorzie <estherannorzie@gmail.com>
2022-06-14 20:58:04 -0400

    Commented out padding-5 class

--- src/index.js
+++ src/index.js
@@ -4,38 +4,45 @@ const state = {
   temperatureCount: 0,
 };
 
-const changeTemperatureColor = (displayTemperatureEl) => {
-  // if temperature value is 0 put (class)
-  displayTemperatureEl.className = "padding-5";
+
+const changeTemperatureEnvironment = (displayTemperatureEl) => {
+  // displayTemperatureEl.className = "padding-5";
+  const landscape = document.getElementById("landscape");
+
   if (state.temperatureCount <= 49) {
-   displayTemperatureEl.classList.add("teal");
+    displayTemperatureEl.classList.add("teal");
+    landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
   }
   else if (state.temperatureCount <= 59) {
-    displayTemperatureEl.classList.add( "green");
+    displayTemperatureEl.classList.add("green");
   }
   else if (state.temperatureCount <= 69) {
-    displayTemperatureEl.classList.add( "yellow");
+    displayTemperatureEl.classList.add("yellow");
+    landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
   } 
-  else if (state.temperatureCount <= 79) { displayTemperatureEl.classList.add( "orange");
+  else if (state.temperatureCount <= 79) { 
+    displayTemperatureEl.classList.add("orange");
+    landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
   }
   else {
-   displayTemperatureEl.classList.add( "red");
- }
+    displayTemperatureEl.classList.add("red");
+    landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
+  }
 };
 
-const increaseTemperatureCount = (tempChangeValue) => {
+const incOrDecTemperatureCount = (tempChangeValue) => {
   const displayTemperatureEl = document.getElementById("displayTemperature");
   state.temperatureCount += tempChangeValue;
-  changeTemperatureColor(displayTemperatureEl);
+  changeTemperatureEnvironment(displayTemperatureEl);
   displayTemperatureEl.textContent = `Temperature: ${state.temperatureCount}`;
 };
 
 const registerEventHandlers = () => {
   const upArrowEl = document.getElementById("upArrow");
-  upArrowEl.addEventListener("click", () => { increaseTemperatureCount(1)});
+  upArrowEl.addEventListener("click", () => {incOrDecTemperatureCount(1)});
 
   const downArrowEl = document.getElementById("downArrow");
-  downArrowEl.addEventListener("click", () => { increaseTemperatureCount(-1)});
+  downArrowEl.addEventListener("click", () => {incOrDecTemperatureCount(-1)});
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
