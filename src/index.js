baced460f47e769f1d30f01789b83bc5a23ed179
Esther Annorzie <estherannorzie@gmail.com>
2022-06-16 11:05:29 -0400

    Added event listener to watch for city input

--- src/index.js
+++ src/index.js
@@ -34,19 +34,32 @@ const changeTemperatureEnvironment = (displayTemperatureEl) => {
   }
 };
 
+
 const incOrDecTemperatureCount = (tempChangeValue) => {
-  const displayTemperatureEl = document.getElementById("displayTemperature");
+  const displayTemperatureEl = document.getElementById('displayTemperature');
   state.temperatureCount += tempChangeValue;
   changeTemperatureEnvironment(displayTemperatureEl);
   displayTemperatureEl.textContent = `Temperature: ${state.temperatureCount}`;
 };
 
-const registerEventHandlers = () => {
-  const upArrowEl = document.getElementById("upArrow");
-  upArrowEl.addEventListener("click", () => {incOrDecTemperatureCount(1)});
 
-  const downArrowEl = document.getElementById("downArrow");
-  downArrowEl.addEventListener("click", () => {incOrDecTemperatureCount(-1)});
+const changeCityName = (input) => {
+  const cityEl = document.getElementById('city');
+  cityEl.textContent = `${input.target.value}`;
 };
+
+
+const registerEventHandlers = () => {
+  const upArrowEl = document.getElementById('upArrow');
+  upArrowEl.addEventListener('click', () => {incOrDecTemperatureCount(1)});
+
+  const downArrowEl = document.getElementById('downArrow');
+  downArrowEl.addEventListener('click', () => {incOrDecTemperatureCount(-1)});
+
+  const inputEl = document.querySelector('input').oninput;
+  // inputEl.oninput = handleInput;
+  cityEl.addEventListener('input', () => {changeCityName(inputEl)})
+};
+
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
