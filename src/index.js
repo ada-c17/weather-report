8b1ba42bd2e299e8a0882eae6e61a1a76b1845c4
Esther Annorzie <estherannorzie@gmail.com>
2022-06-17 12:05:26 -0400

    Removed .env from gitignore, removed unneeded properties from get request params

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
-"use strict";
+'use strict';
 
 const state = {
   temperatureCount: 0,
 };
 
-
 const changeTemperatureEnvironment = (displayTemperatureEl) => {
-  // displayTemperatureEl.className = "padding-5"; 
-  const landscape = document.getElementById("landscape");
+  // displayTemperatureEl.className = "padding-5";
+  const landscape = document.getElementById('landscape');
 
   if (state.temperatureCount <= 49) {
-    displayTemperatureEl.classList.add("teal");
-  }
-  else if (state.temperatureCount <= 59) {
-    displayTemperatureEl.classList.add("green");
-    displayTemperatureEl.classList.remove("teal");
-    landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲"
-  }
-  else if (state.temperatureCount <= 69) {
-    displayTemperatureEl.classList.add("yellow");
-    landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃"
-    displayTemperatureEl.classList.remove("green");
-  } 
-  else if (state.temperatureCount <= 79) { 
-    displayTemperatureEl.classList.add("orange");
-    landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷"
-    displayTemperatureEl.classList.remove("yellow");
-  }
-  else {
-    displayTemperatureEl.classList.add("red");
-    landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂"
-    displayTemperatureEl.classList.remove("orange");
+    displayTemperatureEl.classList.add('teal');
+  } else if (state.temperatureCount <= 59) {
+    displayTemperatureEl.classList.add('green');
+    displayTemperatureEl.classList.remove('teal');
+    landscape.textContent = '🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲';
+  } else if (state.temperatureCount <= 69) {
+    displayTemperatureEl.classList.add('yellow');
+    landscape.textContent = '🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃';
+    displayTemperatureEl.classList.remove('green');
+  } else if (state.temperatureCount <= 79) {
+    displayTemperatureEl.classList.add('orange');
+    landscape.textContent = '🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷';
+    displayTemperatureEl.classList.remove('yellow');
+  } else {
+    displayTemperatureEl.classList.add('red');
+    landscape.textContent = '🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂';
+    displayTemperatureEl.classList.remove('orange');
   }
 };
-
 
 const incOrDecTemperatureCount = (tempChangeValue) => {
   const displayTemperatureEl = document.getElementById('displayTemperature');
@@ -43,48 +37,53 @@ const incOrDecTemperatureCount = (tempChangeValue) => {
   displayTemperatureEl.textContent = `Temperature: ${state.temperatureCount}`;
 };
 
-
 const changeCityName = (event) => {
   const cityEl = document.getElementById('city');
   cityEl.textContent = `${event.target.value}`;
 };
 
 // move the code using axios into a helper function called when the Current Temperature button is clicked
-const showCurrentTemperature = () => {
-  const LOCATION_KEY = process.env['LOCATION_KEY'];
+const showCurrentTemperature = (city) => {
   let latitude, longitude;
-  const cityText = document.getElementById('city').innerHTML;
-
-  axios.get('http://127.0.0.1:5500/location',
-  {
-    params: {
-      key: LOCATION_KEY,
-      q: cityText,
-      format: 'json'
-    }
-  })
-  .then( (response) => {
-    console.log('Success. Location found.');
-    latitude = response.data[0].lat;
-    longitude = response.data[0].lon;
-    console.log(`Latitude: ${latitude}, longitude ${longitude}`);
-  })
-  .catch((error) => {
-    console.log('Error. Location not found.')
-  });
+  axios
+    .get('http://127.0.0.1:5500/location', {
+      params: {
+        q: city,
+      },
+    })
+    .then((response) => {
+      console.log('Success. Location found.');
+      latitude = response.data[0].lat;
+      longitude = response.data[0].lon;
+      console.log(
+        'The value of status inside of response is:',
+        response.status
+      );
+      console.log(
+        'The date inside header inside response is:',
+        response.headers.date
+      );
+      console.log('The data given back by the API response is:', response.data);
+      console.log(`Latitude: ${latitude}, longitude ${longitude}`);
+    })
+    .catch((error) => {
+      console.log('Error. Location not found.');
+    });
 };
-
 
 const registerEventHandlers = () => {
   const upArrowEl = document.getElementById('upArrow');
-  upArrowEl.addEventListener('click', () => {incOrDecTemperatureCount(1)});
+  upArrowEl.addEventListener('click', () => {
+    incOrDecTemperatureCount(1);
+  });
 
   const downArrowEl = document.getElementById('downArrow');
-  downArrowEl.addEventListener('click', () => {incOrDecTemperatureCount(-1)});
+  downArrowEl.addEventListener('click', () => {
+    incOrDecTemperatureCount(-1);
+  });
 
   const inputEl = document.getElementById('city-input');
   inputEl.addEventListener('input', changeCityName);
 };
 
-
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+document.addEventListener('DOMContentLoaded', registerEventHandlers);
