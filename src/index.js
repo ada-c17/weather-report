9c3e35e7351f696205ebd3a6e88cfc9c4903f2c2
shelby <faulconershelby@gmail.com>
2022-06-25 14:01:45 -0700

    added proxy server and made weather button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 
 const state = {
     temp: 23,
+    city: 'city',
+    lat: 'lat',
+    lon: 'lon',
 };
 
 const addTemp = () => {
@@ -25,35 +28,45 @@ const tempThemeToggle = () => {
     lowerTemp();
 
     const tempAdjustElement = document.getElementById("tempAdjust");
-    // const tempElement = document.getElementById("tempBox");
     const tempAdjustLandscape = document.getElementById("landScape");
 
-    console.log(tempAdjustElement)
 
     if (state.temp >= 32){
-        // change background, #tempAdjust,
         tempAdjustElement.className = "tooHot";
-        // and #tempAdjust h2 color -> too hot
-        // tempElement.className = "tooHot";
         tempAdjustLandscape.className = "landHot";
     } else if (state.temp >= 25 && state.temp < 32){
-        // change background, #tempAdjust,
         tempAdjustElement.className = "hot";
-        // and #tempAdjust h2 color -> hot
-        // tempElement.className = "hot";
+        tempAdjustLandscape.className = "landTemperate";
     } else if (state.temp > 0 && state.temp <= 11) {
-        // change background, #tempAdjust,
         tempAdjustElement.className = "cold";
-        // and #tempAdjust h2 color -> cold
-        // tempElement.className = "cold";
         tempAdjustLandscape.className = "landCold";
     } else if (state.temp <= 0){
-        // change background, #tempAdjust,
         tempAdjustElement.className = "freezing";
-        // and #tempAdjust h2 color -> freezing
-        // tempElement.className = "freezing";
         tempAdjustLandscape.className = "landFreezing";
     };
+};
+
+const cityNameResponse = () => {
+
+    const inputCity = document.getElementById('cityText').value;
+    const cityDisplay = document.getElementById('city');
+    state.city = inputCity
+    cityDisplay.textContent = state.city;
+};
+
+const getLatAndLong = (place) => {
+    return axios
+    .get('https://us1.locationiq.com/v1/search.php',{
+    params: {
+    q: state.city,
+    },
+    }).then((response) => {
+      // console.log(response.data);
+    const lat = response.data[0]['lat']
+    const lon = response.data[0]['lon']
+    return {lat, lon} 
+    })
+    .catch(err => console.log({err}))
 };
 
 const registerEventHandlers = () => {
@@ -65,7 +78,30 @@ const registerEventHandlers = () => {
     tempDownButton.addEventListener("click", lowerTemp);
     tempDownButton.addEventListener("click", tempThemeToggle);
 
+    // add event handler & listener for cityNameResponse change
+    // for input first
+    // eventually for irlTemp button and keyboard  `enter`
+    const cityNameInput = document.querySelector("#cityText");
+    cityNameInput.addEventListener("change", cityNameResponse);
+
 };
+
+const show_sky = () => {
+    let weather = document.getElementById("sky_id");
+
+    if (weather.style.display == "block") {
+        weather.style.display == "none";
+    } else {
+        weather.style.display = "block";
+    };
+};
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropdown_button')) {
+        document.getElementById('sky_id')
+            .style.display = "none";
+    };
+}
 
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
