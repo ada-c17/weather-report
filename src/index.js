48f7b9d3d35caaddbe315ea5fb39304046de2f7e
Jande Royalty <janderoyalty@gmail.com>
2023-07-13 01:37:13 -0500

    creats an elements section to streamline the code. Adds more images for the sky

--- src/index.js
+++ src/index.js
@@ -1,90 +1,119 @@
-// import 'regenerator-runtime/runtime';
-// import axios from 'axios';
+// ********** ELEMENTS ***********
+const midColor = document.getElementById('mid--color');
+const sky = document.getElementById('sky--select-box');
+const skyImage = document.getElementById('sky');
+const land = document.getElementById('land');
+const tempContainer = document.getElementById('temperature');
+const locationInputText = document.getElementById('location-input');
+const newLocationName = document.getElementById(
+  'location-display--display-name'
+);
+const dlProgressContainer = document.getElementById(
+  'downloading-progress-text'
+);
+const tempInc = document.getElementById('arrow-increase');
+const tempDec = document.getElementById('arrow-decrease');
+const locationSubmit = document.getElementById('location-submit');
+const locationReset = document.getElementById('location-reset');
 
+// ********** STATE ***********
 const state = {
   temp: 70,
-  color: document.getElementById('mid--color'),
-  skyCondition: document.getElementById('sky--select-box'),
-  skyConditionImage: document.getElementById('sky'),
-  land: document.getElementById('land'),
+  color: midColor,
+  sky,
+  skyImage,
+  land,
   location: '',
   lat: 0,
   lon: 0,
-  downloading: true,
 };
 
 // ********** SKY changes ***********
-// change sky
-const changeSky = () => {
-  // SUNNY
-  if (state.skyCondition.value === 'sunny') {
-    state.skyConditionImage.ariaLabel = 'outline of hills with yellow sunset';
-
-    state.skyConditionImage.title = 'Photo by Nic Y-C on Unsplash';
-
-    state.skyConditionImage.style.background =
-      "url('/assets/sky/sunny 1920x360.jpg')";
-
-    // RAINY
-  } else if (state.skyCondition.value === 'rainy') {
-    state.skyConditionImage.ariaLabel = 'tree branches in a rain storm';
-
-    state.skyConditionImage.title = 'Photo by Milin John on Unsplash';
-
-    state.skyConditionImage.style.background =
-      "url('/assets/sky/rainy 1920x360.jpg')";
-
-    // CLOUDY
-  } else if (state.skyCondition.value === 'cloudy') {
-    state.skyConditionImage.ariaLabel = 'dark sky with fluffy dark clouds';
-
-    state.skyConditionImage.title = 'Photo by Harshit Sharma on Unsplash';
-
-    state.skyConditionImage.style.background =
-      "url('/assets/sky/cloudy 1920x360.jpg')";
-
-    // SNOWY
-  } else if (state.skyCondition.value === 'snowy') {
-    state.skyConditionImage.ariaLabel =
-      'ice covered tree branch covered snow falling our of sky';
-
-    state.skyConditionImage.title = 'Photo by Chandler Cruttenden on Unsplash';
-
-    state.skyConditionImage.style.background =
-      "url('/assets/sky/snowy 1920x360.jpg')";
-
-    // CLEAR
-  } else if (state.skyCondition.value === ' ') {
-    state.skyConditionImage.ariaLabel = 'clear blue skies';
-
-    state.skyConditionImage.title = 'Photo by Patrick Fore on Unsplash';
-
-    state.skyConditionImage.style.background =
-      "url('/assets/sky/clear 1920x360.jpg')";
-  }
-};
-
-const validateSkyCondition = (condition) => {
+// validate sky
+const validatesky = (condition) => {
   if (condition === 'Clear') {
-    state.skyCondition.value = 'sunny';
+    state.sky.value = 'sunny';
   } else if (
     condition === 'Rain' ||
     condition === 'Drizzle' ||
     condition === 'Thunderstorm'
   ) {
-    state.skyCondition.value = 'rainy';
+    state.sky.value = 'rainy';
   } else if (condition === 'Clouds') {
-    state.skyCondition.value = 'cloudy';
+    state.sky.value = 'cloudy';
   } else if (condition === 'Snow') {
-    state.skyCondition.value = 'snowy';
+    state.sky.value = 'snowy';
+  } else if (condition === 'Foggy') {
+    state.sky.value = 'foggy';
+  } else if (condition === 'Clear') {
+    state.sky.value = 'clear';
+  } else if (condition === 'Tornado') {
+    state.sky.value = 'tornado';
+  } else {
+    state.sky.value = 'other';
   }
   changeSky();
+};
+
+// change sky
+const changeSky = () => {
+  // SUNNY
+  if (state.sky.value === 'sunny') {
+    state.skyImage.ariaLabel = 'outline of hills with yellow sunset';
+    state.skyImage.title = 'Photo by Nic Y-C on Unsplash';
+    state.skyImage.style.background = "url('/assets/sky/sunny 1920x360.jpg')";
+
+    // RAINY
+  } else if (state.sky.value === 'rainy') {
+    state.skyImage.ariaLabel = 'tree branches in a rain storm';
+    state.skyImage.title = 'Photo by Milin John on Unsplash';
+    state.skyImage.style.background = "url('/assets/sky/rainy 1920x360.jpg')";
+
+    // CLOUDY
+  } else if (state.sky.value === 'cloudy') {
+    state.skyImage.ariaLabel = 'dark sky with fluffy dark clouds';
+    state.skyImage.title = 'Photo by Harshit Sharma on Unsplash';
+    state.skyImage.style.background = "url('/assets/sky/cloudy 1920x360.jpg')";
+
+    // SNOWY
+  } else if (state.sky.value === 'snowy') {
+    state.skyImage.ariaLabel =
+      'ice covered tree branch covered snow falling our of sky';
+    state.skyImage.title = 'Photo by Chandler Cruttenden on Unsplash';
+    state.skyImage.style.background = "url('/assets/sky/snowy 1920x360.jpg')";
+
+    // TORNADO
+  } else if (state.sky.value === 'tornado') {
+    state.skyImage.ariaLabel =
+      'dark sky with tornado to the left and lightning to the right over green hills';
+    state.skyImage.title = 'Photo by Ralph W. lambrecht on Pexels';
+    state.skyImage.style.background = "url('/assets/sky/tornado 1920x360.jpg')";
+
+    // FOGGY
+  } else if (state.sky.value === 'foggy') {
+    state.skyImage.ariaLabel =
+      'dark sky with tornado to the left and lightning to the right over green hills';
+    state.skyImage.title = 'Photo by Axel Holen on Unsplash';
+    state.skyImage.style.background = "url('/assets/sky/foggy 1920x360.jpg')";
+
+    // CLEAR
+  } else if (state.sky.value === 'clear' || state.sky.value === ' ') {
+    state.skyImage.ariaLabel = 'clear blue skies';
+    state.skyImage.title = 'Photo by Patrick Fore on Unsplash';
+    state.skyImage.style.background = "url('/assets/sky/clear 1920x360.jpg')";
+
+    // OTHER
+  } else {
+    state.skyImage.ariaLabel =
+      'dark sky with tornado to the left and lightning to the right over green hills';
+    state.skyImage.title = 'Photo by Xiomara R.';
+    state.skyImage.style.background = "url('/assets/sky/other 1920x360.jpg')";
+  }
 };
 
 // ********** TEMP changes ***********
 // change TEMP displayed
 const changeTempDisplay = () => {
-  const tempContainer = document.getElementById('temperature');
   tempContainer.textContent = `${state.temp}°F`;
 };
 
@@ -134,11 +163,7 @@ const changeColorTemp = () => {
 // **********LOCATION INPUT changes ***********
 // location name updates in REAL TIME
 const changeLocationDisplay = () => {
-  const inputText = document.getElementById('location-input');
-  const newLocationName = document.getElementById(
-    'location-display--display-name'
-  );
-  newLocationName.textContent = inputText.value;
+  newLocationName.textContent = locationInputText.value;
 };
 
 // location NAME UPDATES with RETURN key
@@ -150,32 +175,19 @@ const returnKeySubmitLocationInput = (event) => {
 
 // location NAME UPDATES with SUBMIT button
 const submitLocationInput = () => {
-  state.location = document.getElementById('location-input').value;
+  state.location = locationInputText.value;
   getLatLon();
-};
-
-// location loading text
-const changeWeatherLoading = () => {
-  const dlProgressContainer = document.getElementById(
-    'downloading-progress-text'
-  );
-  dlProgressContainer.textContent = `Getting ${state.location} weather`;
 };
 
 // ******** RESET *********
 // location NAME RESET
 const resetInput = () => {
-  const newLocationName = document.getElementById(
-    'location-display--display-name'
-  );
-  document.getElementById('land').style.background =
-    "url('/assets/temp/default 1920x360.jpg')";
+  state.land.style.background = "url('/assets/temp/default 1920x360.jpg')";
   state.color.style.backgroundColor = 'rgba(255,255,255, 0.5)';
-  const inputText = document.getElementById('location-input');
 
-  inputText.value = '';
+  locationInputText.value = '';
   newLocationName.textContent = 'Earth';
-  state.skyCondition.value = ' ';
+  state.sky.value = ' ';
   state.temp = 70;
   changeTempDisplay();
   changeSky();
@@ -184,9 +196,6 @@ const resetInput = () => {
 // ********API CALLS*********
 // get LOCATION info
 const getLatLon = () => {
-  const dlProgressContainer = document.getElementById(
-    'downloading-progress-text'
-  );
   dlProgressContainer.textContent = `Getting ${state.location} weather`;
   axios
     .get('https://weather-report-proxy-server-7n03.onrender.com/location', {
@@ -203,7 +212,8 @@ const getLatLon = () => {
     })
     .catch((error) => {
       console.log(error);
-      dlProgressContainer.textContent = 'Error occurred while fetching weather';
+      dlProgressContainer.textContent =
+        'Error occurred while fetching location coordinates';
     });
 };
 
@@ -217,48 +227,39 @@ const getLocationWeather = (progress) => {
       },
     })
     .then((response) => {
+      console.log(response.data.current.weather[0].main);
       condition = response.data.current.weather[0].main;
       kelvinTemp = response.data.current.temp;
     })
     .then((response) => {
-      validateSkyCondition(condition);
+      validatesky(condition);
       convertKelvin(kelvinTemp);
-      progress.innerHTML = ''; // Clear loading message
+      progress.innerHTML = '';
       progress.appendChild(document.createElement('br'));
       changeColorTemp();
     })
     .catch((error) => {
       console.log(error);
-      progress.textContent = 'Error occurred while fetching weather';
+      progress.textContent = 'Error occurred while fetching weather conditions';
     });
 };
 
 const registerEventHandlers = () => {
   // change sky
-  const makeSkyChange = document.getElementById('sky--select-box');
-  makeSkyChange.addEventListener('change', changeSky);
+  sky.addEventListener('change', changeSky);
   // increase temp
-  const tempInc = document.getElementById('arrow-increase');
   tempInc.addEventListener('click', incTemp);
   tempInc.addEventListener('click', changeColorTemp);
   // decrease temp
-  const tempDec = document.getElementById('arrow-decrease');
   tempDec.addEventListener('click', decTemp);
   tempDec.addEventListener('click', changeColorTemp);
   // change location - SUBMIT button
-  const locationSubmit = document.getElementById('location-submit');
   locationSubmit.addEventListener('click', submitLocationInput);
   // change location - RETURN key
-  const enterKeyLocationSubmit = document.getElementById('location-input');
-  enterKeyLocationSubmit.addEventListener(
-    'keyup',
-    returnKeySubmitLocationInput
-  );
+  locationInputText.addEventListener('keyup', returnKeySubmitLocationInput);
   // change location - NAME DISPLAY
-  const locationInput = document.getElementById('location-input');
-  locationInput.addEventListener('keyup', changeLocationDisplay);
+  locationInputText.addEventListener('keyup', changeLocationDisplay);
   // reset page - RESET button
-  const locationReset = document.getElementById('location-reset');
   locationReset.addEventListener('click', resetInput);
 };
 
